2ad31b91ee06877aea46543dee62d69fcb62ebcc
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-13 00:07:13 +0000

    * Egg-building infrastructure improvements (adding README.txt, LICENSE.txt, setup.cfg, etc.; re-ordering setup keywords to be alphabetical; updating dependencies to use the etsdeps function to allow dev builds to match; etc.)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension, find_packages
-
 from numpy import get_include
+
 
 contour = Extension(
     'enthought.chaco2.contour.contour',
@@ -9,24 +9,37 @@ contour = Extension(
     define_macros=[('NUMPY', None)]
 )
 
+
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
+
+
+# Declare our ETS project dependencies.
+ENABLE2 = etsdep('enthought.enable2', '2.0.0b1')
+ETSCONIFG = etsdep('enthought.etsconfig', '2.0.0b1')
+KIVA = etsdep('enthought.kiva', '2.0.0b1')
+TRAITS = etsdep('enthought.traits', '2.0.0b1')
+
+
 setup(
-    name = 'enthought.chaco2',
-    version='3.0a1',
-    description  = 'Chaco plotting toolkit',
-    author       = 'Enthought, Inc',
+    author = 'Enthought, Inc',
     author_email = 'info@enthought.com',
-    url          = 'http://code.enthought.com/chaco',
-    license      = 'BSD',
-    zip_safe     = False,
-    packages = find_packages(),
-    ext_modules = [contour],
-    include_package_data = True,
-    install_requires = [
-        "enthought.etsconfig",
-        "enthought.traits",
-        "enthought.kiva",
-        "enthought.enable2",
-    ],
+    dependency_links = [
+        'http://code.enthought.com/enstaller/eggs/source',
+        'http://code.enthought.com/enstaller/eggs/source/unstable',
+        ],
+    description = 'Chaco plotting toolkit',
     extras_require = {
         # All non-ets dependencies should be in this extra to ensure users can
         # decide whether to require them or not.
@@ -34,11 +47,21 @@ setup(
             'numpy >=1.0.2',
             ],
         },
+    ext_modules = [contour],
+    include_package_data = True,
+    install_requires = [
+        ENABLE2,
+        ETSCONFIG,
+        KIVA,
+        TRAITS,
+        ],
+    license = 'BSD',
+    name = 'enthought.chaco2',
     namespace_packages = [
         "enthought",
         ],
     packages = find_packages(
-        exclude=['docs', 'examples', 'examples.zoomed_plot']
+        exclude=['docs', 'examples', 'examples.zoomed_plot'],
         ),
     tests_require = [
         'nose >= 0.9',
