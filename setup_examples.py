347317629dfc86ac5d65d720fa855f141aae95ae
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-12-21 22:37:38 +0000

    adding examples.cfg and setup_examples.py for Chaco

---
+++ setup_examples.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    author = 'Enthought, Inc',
+    author_email = 'info@enthought.com',
+    description = 'Chaco plotting toolkit (Examples)',
+
+    license = 'BSD',
+    packages = [],
+    examples_map = 'examples/examples.cfg',
+    name = "chaco-examples",
+    url = 'http://code.enthought.com/chaco',
+    version='3.0.0a1',
+    zip_safe = False,
+    )
+
