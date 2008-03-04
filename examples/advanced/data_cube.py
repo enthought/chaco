d1984c602b83bcb5694031c020c9eb343337b1c1
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-04 04:45:10 +0000

    changing chaco example to use enable2.example_support instead of chaco2.example_support

--- examples/advanced/data_cube.py
+++ examples/advanced/data_cube.py
@@ -22,7 +22,7 @@ from enthought.chaco2.api import ArrayPlotData, Plot, GridPlotContainer, \
                                  BaseTool, DataRange1D
 from enthought.chaco2.default_colormaps import *
 from enthought.chaco2.tools.api import LineInspector, SimpleZoom
-from enthought.chaco2.example_support import DemoFrame, demo_main
+from enthought.enable2.example_support import DemoFrame, demo_main
 from enthought.enable2.api import Window
 from enthought.traits.api import Any, Array, Bool, Callable, CFloat, CInt, \
         Event, Float, HasTraits, Int, Trait, on_trait_change
