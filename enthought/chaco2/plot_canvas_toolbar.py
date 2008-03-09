35864ec6d3f6a7c8a490b1b723203d7a01c347a0
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-09 04:10:14 +0000

    making plot_canvas_toolbar subclass from a plot container

--- enthought/chaco2/plot_canvas_toolbar.py
+++ enthought/chaco2/plot_canvas_toolbar.py
@@ -1,41 +1,52 @@
 
+from enthought.traits.api import Enum, Int
 from enthought.enable2.drawing.api import DrawingCanvasToolbar, ToolbarButton
 
+# Local, relative imports
+from plot_containers import HPlotContainer, VPlotContainer
 from plot_component import PlotComponent
 
 
-class PlotCanvasToolbar(PlotComponent, DrawingCanvasToolbar):
+class PlotCanvasToolbar(VPlotContainer, DrawingCanvasToolbar):
+
+    spacing = Int(5)
+
+    align = Enum("ur", "ul", "ll", "lr")
+
+    #fit_components = "v"
     
+    # Override some inherited traits
     resizable = ""
-    fill_padding = True
+    fill_padding = False
     unified_draw = True
 
-    def add_button(self, *args):
-        DrawingCanvasToolbar.add_button(self, *args)
-        self._layout_needed = True
+    #def add_button(self, *args):
+    #    DrawingCanvasToolbar.add_button(self, *args)
+    #    self._layout_needed = True
 
-    def get_preferred_size(self):
-        width = self.hpadding + self.button_spacing * (len(self.components) - 1)
-        height = 0
-        for c in self.components:
-            w, h = c.get_preferred_size()
-            width += w
-            if h > height:
-                height = h
-        height += self.vpadding
-        return width, height
+    #def get_preferred_size(self):
+    #    width = self.hpadding + 2*self.margin + self.button_spacing * (len(self.components) - 1)
+    #    height = 2 * self.margin
+    #    for c in self.components:
+    #        w, h = c.get_preferred_size()
+    #        width += w
+    #        if h > height:
+    #            height = h
+    #    height += self.vpadding
+    #    return width, height
 
-    def _do_layout(self):
-        x = 0
-        y = 0
-        for c in self.components:
-            c.outer_position = [x,y]
-            x += self.button_spacing + c.outer_width
-        return
+    def overlay(self, component, gc, view_bounds=None, mode="normal"):
+        """ Allows the toolbar to behave like an overlay """
+        pass
 
-    def _draw_plot(self, *args, **kw):
-        return self._draw_mainlayer(*args, **kw)
-    
+    #def _do_layout(self):
+    #    x = self.margin
+    #    y = self.margin
+    #    for c in self.components:
+    #        c.outer_position = [x,y]
+    #        x += self.button_spacing + c.outer_width
+    #    return
+
     def _canvas_changed(self):
         # Override the one we inherit from CrawingCanvasToolbar
         return
