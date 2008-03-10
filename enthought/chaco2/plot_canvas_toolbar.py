71254a6ca5c2d07b966e1384b540b047ea3ac813
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-10 18:06:39 +0000

    removing dead code in plot canvas toolbar

--- enthought/chaco2/plot_canvas_toolbar.py
+++ enthought/chaco2/plot_canvas_toolbar.py
@@ -13,39 +13,16 @@ class PlotCanvasToolbar(VPlotContainer, DrawingCanvasToolbar):
 
     align = Enum("ur", "ul", "ll", "lr")
 
-    #fit_components = "v"
+    fit_components = "v"
     
     # Override some inherited traits
-    resizable = ""
+    resizable = "v"
     fill_padding = False
     unified_draw = True
-
-    #def add_button(self, *args):
-    #    DrawingCanvasToolbar.add_button(self, *args)
-    #    self._layout_needed = True
-
-    #def get_preferred_size(self):
-    #    width = self.hpadding + 2*self.margin + self.button_spacing * (len(self.components) - 1)
-    #    height = 2 * self.margin
-    #    for c in self.components:
-    #        w, h = c.get_preferred_size()
-    #        width += w
-    #        if h > height:
-    #            height = h
-    #    height += self.vpadding
-    #    return width, height
 
     def overlay(self, component, gc, view_bounds=None, mode="normal"):
         """ Allows the toolbar to behave like an overlay """
         pass
-
-    #def _do_layout(self):
-    #    x = self.margin
-    #    y = self.margin
-    #    for c in self.components:
-    #        c.outer_position = [x,y]
-    #        x += self.button_spacing + c.outer_width
-    #    return
 
     def _canvas_changed(self):
         # Override the one we inherit from CrawingCanvasToolbar
