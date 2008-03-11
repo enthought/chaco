2d6b0c3a2582ff9e2fdabeaf055110e429e0606f
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-11 21:53:20 +0000

    simplifying noninteractive.py example, adding dpi settings to it

--- examples/noninteractive.py
+++ examples/noninteractive.py
@@ -5,72 +5,60 @@ file on disk.
 """
 
 # Major library imports
-from numpy import arange, fabs, pi, sin
+from numpy import fabs, linspace, pi, sin
 from scipy.special import jn
 
 # Enthought library imports
-from enthought.kiva.backend_image import GraphicsContext
 from enthought.traits.api import false
 
 # Chaco imports
-from enthought.chaco2.api import OverlayPlotContainer, create_line_plot, \
-                                 add_default_axes, add_default_grids
+from enthought.chaco2.api import ArrayPlotData, Plot, PlotGraphicsContext
+from enthought.chaco2.example_support import COLOR_PALETTE
+
+DPI = 72.0
+
+# This is a bit of a hack, to work around the fact that line widths don't scale
+# with the GraphicsContext's CTM.
+dpi_scale = DPI / 72.0
 
 def create_plot():
-    container = OverlayPlotContainer(padding = 50, fill_padding = True,
-                                     bgcolor = "lightgray")
-
     numpoints = 100
     low = -5
     high = 15.0
-    x = arange(low, high+0.001, (high-low)/numpoints)
-
-    # Plot some bessel functions
-    value_mapper = None
-    index_mapper = None
+    x = linspace(low, high, numpoints)
+    pd = ArrayPlotData(index=x)
+    p = Plot(pd, bgcolor="lightgray", padding=50, border_visible=True)
     for i in range(10):
-        y = jn(i, x)
-        plot = create_line_plot((x,y), color=(1.0-i/10.0,i/10.0,0,1), width=2.0,
-                                index_sort="ascending")
-        if i == 0:
-            value_mapper = plot.value_mapper
-            index_mapper = plot.index_mapper
-            add_default_axes(plot)
-            add_default_grids(plot)
-        else:
-            plot.value_mapper = value_mapper
-            value_mapper.range.add(plot.value)
-            plot.index_mapper = index_mapper
-            index_mapper.range.add(plot.index)
-
-        if i%2 == 1:
-            plot.line_style = "dash"
-        container.add(plot)
-
-    return container
-
+        pd.set_data("y" + str(i), jn(i,x))
+        p.plot(("index", "y" + str(i)), color=tuple(COLOR_PALETTE[i]),
+               width = 2.0 * dpi_scale)
+    p.x_grid.visible = True
+    p.x_grid.line_width *= dpi_scale
+    p.y_grid.visible = True
+    p.y_grid.line_width *= dpi_scale
+    p.legend.visible = True
+    return p
 
 def draw_plot(filename, size=(800,600)):
     container = create_plot()
-    container.bounds = list(size)
+    container.outer_bounds = list(size)
     container.do_layout(force=True)
-    gc = GraphicsContext((size[0]+1, size[1]+1))
-    container.draw(gc)
+    gc = PlotGraphicsContext(size, dpi=DPI)
+    gc.render_component(container)
     gc.save(filename)
     return
 
 def draw_pdf(filename, size=(800,600)):
+    from enthought.chaco2.pdf_graphics_context import PdfPlotGraphicsContext
     container = create_plot()
     container.bounds = list(size)
     container.do_layout(force=True)
-    
-    from enthought.chaco2.pdf_graphics_context import PdfPlotGraphicsContext
     gc = PdfPlotGraphicsContext(filename=filename, dest_box = (0.5, 0.5, 5.0, 5.0))
     gc.render_component(container)
     gc.save()
 
 if __name__ == "__main__":
-    draw_plot("noninteractive.png")
+    draw_plot("noninteractive.png", size=(800, 600))
     
     # If you have ReportLab installed, you can uncomment the following:
     #draw_pdf("noninteractive.pdf")
