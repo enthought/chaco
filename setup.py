8e18f96a0e85e8b21d54166da1b901f371b6548b
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-08 19:32:45 +0000

    factoring chaco's setup.py into a setup_data.py on the trunk

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, Extension, find_packages
 from numpy import get_include
-
+from setup_data import INFO
 
 contour = Extension(
     'enthought.chaco2.contour.contour',
@@ -16,30 +16,6 @@ speedups = Extension(
     define_macros=[('NUMPY', None)]
     )
 
-
-# Function to convert simple ETS project names and versions to a requirements
-# spec that works for both development builds and stable builds.  Allows
-# a caller to specify a max version, which is intended to work along with
-# Enthought's standard versioning scheme -- see the following write up:
-#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
-def etsdep(p, min, max=None, literal=False):
-    require = '%s >=%s.dev' % (p, min)
-    if max is not None:
-        if literal is False:
-            require = '%s, <%s.a' % (require, max)
-        else:
-            require = '%s, <%s' % (require, max)
-    return require
-
-
-# Declare our ETS project dependencies.
-ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.0b1')
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0b1')
-#TRAITSBACKENDQT -- not needed due to the way it is used in chaco2_plot_container_editor.py
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.0b1')  # -- directly imported by chaco2_plot_editor.py
-TRAITS_UI = etsdep('Traits[ui]', '3.0.0b1')
-
-
 setup(
     author = 'Enthought, Inc',
     author_email = 'info@enthought.com',
@@ -47,30 +23,12 @@ setup(
         'http://code.enthought.com/enstaller/eggs/source',
         ],
     description = 'Chaco plotting toolkit',
-    extras_require = {
-        'wx': [
-            TRAITSBACKENDWX,
-            ],
-
-        # All non-ets dependencies should be in this extra to ensure users can
-        # decide whether to require them or not.
-        'nonets': [
-            'numpy >=1.0.2',
-            #'PyQt4', -- not really required by everyone.
-            'reportlab',
-            'scipy',
-            #'wxPython', -- not really required by everyone.
-            ],
-        },
+    extras_require = INFO["extras_require"],
     ext_modules = [contour, speedups],
     include_package_data = True,
-    install_requires = [
-        ENABLE_TRAITS,
-        ENTHOUGHTBASE,
-        TRAITS_UI,
-        ],
+    install_requires = INFO["install_requires"],
     license = 'BSD',
-    name = 'Chaco',
+    name = INFO["name"],
     namespace_packages = [
         "enthought",
         ],
@@ -84,7 +42,7 @@ setup(
         ],
     test_suite = 'nose.collector',
     url = 'http://code.enthought.com/chaco',
-    version='3.0.0b1',
+    version = INFO["version"],
     zip_safe = False,
     )
 
---
+++ setup_data.py
@@ -0,0 +1,47 @@
+
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
+
+
+# Declare our ETS project dependencies.
+ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.0b1')
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0b1')
+#TRAITSBACKENDQT -- not needed due to the way it is used in chaco2_plot_container_editor.py
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.0b1')  # -- directly imported by chaco2_plot_editor.py
+TRAITS_UI = etsdep('Traits[ui]', '3.0.0b1')
+
+INFO = {
+    "extras_require": {
+        'wx': [
+            TRAITSBACKENDWX,
+            ],
+
+        # All non-ets dependencies should be in this extra to ensure users can
+        # decide whether to require them or not.
+        'nonets': [
+            'numpy >=1.0.2',
+            #'PyQt4', -- not really required by everyone.
+            'reportlab',
+            'scipy',
+            #'wxPython', -- not really required by everyone.
+            ],
+        },
+    "install_requires": [
+        ENABLE_TRAITS,
+        ENTHOUGHTBASE,
+        TRAITS_UI,
+        ],
+    "name": 'Chaco',
+    "version": '3.0.0b1',
+    }
