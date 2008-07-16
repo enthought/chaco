bd47bd24a9ad3a899633a94aff3ed8d25489eb6a
dmartin <dmartin@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-16 19:05:26 +0000

    Changed how the example assembles the path to the image 'capitol.jpg' so that the example can be run from outside of the example directory.

--- examples/shell/imshow.py
+++ examples/shell/imshow.py
@@ -1,17 +1,22 @@
+#Standard library imports
+import sys
+from os import path
 
-#imports
-from numpy import *
-from enthought.chaco2.shell import *
-from enthought.chaco2.api import ArrayPlotData, ImageData
+#Enthought library imports
+from enthought.chaco2.shell import imread, imshow, title, show
+
+#Assemble the path to the image
+example_dir = sys.path[0]
+image_path = path.join(example_dir, '..', 'basic', 'capitol.jpg')
 
 #Create data
-image = imread("../basic/capitol.jpg")
+image = imread(image_path)
 
 #Create plot
 imshow(image, origin="top left")
 
 #Alternatively, call 
-#imshow("../basic/capitol.jpg")
+#imshow(image_path)
 
 #Add a title
 title("simple image plot")
