5932860a9758954c60c17daf03779f4482413b45
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-22 21:12:14 +0000

    Added more generalized make_docs.py. Modified setup.py to pass version info to sphinx during doc build.

--- setup.py
+++ setup.py
@@ -49,6 +49,8 @@ def generate_docs():
                 'target': target,
                 'verbose': True,
                 'versioned': False,
+                'version': INFO['version'],
+                'release': INFO['version']
                 }, [])
             del build
         except:
