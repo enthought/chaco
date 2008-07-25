1ed11410309dba0ea15907299480c62cc6d31b87
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 20:25:59 +0000

    Modified proxy file to have same importable names as original

--- enthought/chaco2/default_colormaps.py
+++ enthought/chaco2/default_colormaps.py
@@ -1,2 +1,22 @@
 # proxy    
 from enthought.chaco.default_colormaps import *
+from enthought.chaco.default_colormaps import fix
+
+# Colormaps, same order as original
+from enthought.chaco.default_colormaps import autumn, bone, copper, flag, gray
+from enthought.chaco.default_colormaps import yarg, hot, hsv, jet, pink, prism
+from enthought.chaco.default_colormaps import spring, summer, winter, cw1_004
+from enthought.chaco.default_colormaps import cw1_005, cw1_006, cw1_028
+from enthought.chaco.default_colormaps import gmt_drywet
+from enthought.chaco.default_colormaps import Blues, BrBG, BuGn, BuPu, GnBu
+from enthought.chaco.default_colormaps import Greens, Greys, OrRd, Oranges
+from enthought.chaco.default_colormaps import PRGn, PiYG, PuBu, PuBuGn, PuOr
+from enthought.chaco.default_colormaps import PuRd, Purples, RdBu, RdGy, RdPu
+from enthought.chaco.default_colormaps import RdYlBu, RdYlGn, Reds, Spectral
+from enthought.chaco.default_colormaps import YlGn, YlGnBu, YlOrBu, YlOrBr
+from enthought.chaco.default_colormaps import YlOrRd, gist_earth, gist_gray
+from enthought.chaco.default_colormaps import gist_heat, gist_ncar, gist_rainbow
+from enthought.chaco.default_colormaps import gist_stern, gist_yarg
+
+__all__ = ['reverse', 'center', 'color_map_functions', 'color_map_dict',
+            'color_map_name_dict']
