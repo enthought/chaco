772433343ac71402f5b8c5f713284ffda7fb1ac5
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 20:31:16 +0000

    Modified proxy file to have same importable names as original

--- enthought/chaco2/scales/formatters.py
+++ enthought/chaco2/scales/formatters.py
@@ -1,2 +1,7 @@
 # proxy    
 from enthought.chaco.scales.formatters import *
+from enthought.chaco.scales.formatters strftimeEx
+
+__all__ = ['NullFormatter', 'BasicFormatter', 'IntegerFormatter',
+           'OffsetFormatter', 'TimeFormatter']
+
