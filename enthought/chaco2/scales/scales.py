109a2a50428858a3d110dada3c5e971f9ea86833
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 20:29:01 +0000

    Modified proxy file to have same importable names as original

--- enthought/chaco2/scales/scales.py
+++ enthought/chaco2/scales/scales.py
@@ -1,2 +1,6 @@
 # proxy    
 from enthought.chaco.scales.scales import *
+from enthought.chaco.scales.scales import _nice
+
+__all__ = ["AbstractScale", "DefaultScale", "FixedScale", "Pow10Scale",
+           "LogScale", "ScaleSystem", "heckbert_interval", "frange"]
