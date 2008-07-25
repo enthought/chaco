430b4f1a5d413bd90dd50858b1d87d51ee4b6d29
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 20:39:17 +0000

    Modified proxy file to have same importable names as original

--- enthought/chaco2/scales/safetime.py
+++ enthought/chaco2/scales/safetime.py
@@ -1,2 +1,7 @@
 # proxy    
 from enthought.chaco.scales.safetime import *
+from enthought.chaco.scales.safetime import mktime, doy, struct_time, localtime
+
+__all__ = ([x for x in dir(stdlib_time) if not x.startswith('_')]
+            + ['safe_fromtimestamp', 'datetime', 'timedelta', 'MINYEAR', 'MAXYEAR',
+               'EPOCH'])
