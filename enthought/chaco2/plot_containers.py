7043dbdda0c3d120b5e24a5f82e8c60aa704c50d
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 19:53:05 +0000

    Modified proxy file to have same importable names as original

--- enthought/chaco2/plot_containers.py
+++ enthought/chaco2/plot_containers.py
@@ -1,2 +1,7 @@
 # proxy    
 from enthought.chaco.plot_containers import *
+from enthought.chaco.plot_containers import DEFAULT_DRAWING_ORDER
+from enthought.chaco.plot_containers import StackedPlotContainer
+
+__all__ = ["OverlayPlotContainer", "HPlotContainer", "VPlotContainer", \
+                   "GridPlotContainer"]
