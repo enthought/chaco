c632003ae394290cf0af51f1b67bcc3f0a1ec500
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 20:35:30 +0000

    Modified proxy file to have same importable names as original

--- enthought/chaco2/scales/time_scale.py
+++ enthought/chaco2/scales/time_scale.py
@@ -1,2 +1,6 @@
 # proxy    
 from enthought.chaco.scales.time_scale import *
+from enthought.chaco.scales.time_scale import td_to_sec
+
+__all__ = ["TimeScale", "CalendarScaleSystem", "HMSScales", "MDYScales",
+           "trange", "tfrac", "dt_to_sec"]
