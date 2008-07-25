e42bab79793538bd34c28abdebf734fb3820a44c
ccasey <ccasey@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-25 22:41:53 +0000

    Removed unecessary import and assignment. This one was checked in accidentally.

--- enthought/chaco2/chaco2_plot_container_editor.py
+++ enthought/chaco2/chaco2_plot_container_editor.py
@@ -1,5 +1,2 @@
 # proxy    
 from enthought.chaco.chaco_plot_container_editor import *
-import enthought.chaco.chaco_plot_container_editor
-
-chaco2_plot_container_editor = chaco_plot_container_editor
