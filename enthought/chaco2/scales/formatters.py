0ceacf46057bb56c637a73b1b980143cc5439484
rkern <rkern@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-26 10:32:51 +0000

    BUG: typo. Thanks, Fred.

--- enthought/chaco2/scales/formatters.py
+++ enthought/chaco2/scales/formatters.py
@@ -1,6 +1,6 @@
 # proxy    
 from enthought.chaco.scales.formatters import *
-from enthought.chaco.scales.formatters strftimeEx
+from enthought.chaco.scales.formatters import strftimeEx
 
 __all__ = ['NullFormatter', 'BasicFormatter', 'IntegerFormatter',
            'OffsetFormatter', 'TimeFormatter']
