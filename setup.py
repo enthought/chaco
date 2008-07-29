4d1c257c7cedf0009790250cdf2f8f66f4b6d0bf
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-29 19:41:29 +0000

    * Added / edited project meta-data in preparation for push into PyPi.

--- setup.py
+++ setup.py
@@ -1,14 +1,34 @@
-import os, zipfile
-from setuptools import setup, Extension, find_packages
-from numpy import get_include
-from setuptools.command.develop import develop
-from distutils.command.build import build as distbuild
+#!/usr/bin/env python
+#
+# Copyright (c) 2008 by Enthought, Inc.
+# All rights reserved.
+#
+
+"""
+Chaco plotting toolkit
+
+<description text needed>
+"""
+
+
 from distutils import log
-from pkg_resources import DistributionNotFound, parse_version, require, VersionConflict 
-
-from setup_data import INFO
+from distutils.command.build import build as distbuild
 from make_docs import HtmlBuild
+from numpy import get_include
+from pkg_resources import DistributionNotFound, parse_version, require, \
+    VersionConflict
+from setup_data import INFO
+from setuptools import setup, Extension, find_packages
+from setuptools.command.develop import develop
+import os
+import zipfile
 
+
+# Pull the description values for the setup keywords from our file docstring.
+DOCLINES = __doc__.split("\n")
+
+
+# Register Python extensions
 contour = Extension(
     'enthought.chaco.contour.contour',
     sources=['enthought/chaco/contour/cntr.c'],
@@ -23,14 +43,16 @@ speedups = Extension(
     define_macros=[('NUMPY', None)]
     )
 
+
+# Function to generate docs from source when building the project.
 def generate_docs():
-    """If sphinx is installed, generate docs.
+    """ If sphinx is installed, generate docs.
     """
     doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'docs')
     source_dir = os.path.join(doc_dir, 'source')
     html_zip = os.path.join(doc_dir,  'html.zip')
     dest_dir = doc_dir
-    
+
     required_sphinx_version = "0.4.1"
     sphinx_installed = False
     try:
@@ -38,21 +60,23 @@ def generate_docs():
         sphinx_installed = True
     except (DistributionNotFound, VersionConflict):
         log.warn('Sphinx install of version %s could not be verified.'
-                    ' Trying simple import...' % required_sphinx_version)
+            ' Trying simple import...' % required_sphinx_version)
         try:
             import sphinx
-            if parse_version(sphinx.__version__) < parse_version(required_sphinx_version):
-                log.error("Sphinx version must be >=%s." % required_sphinx_version)
+            if parse_version(sphinx.__version__) < parse_version(
+                required_sphinx_version):
+                log.error("Sphinx version must be >=" +\
+                    "%s." % required_sphinx_version)
             else:
                 sphinx_installed = True
         except ImportError:
             log.error("Sphnix install not found.")
-    
-    if sphinx_installed:             
+
+    if sphinx_installed:
         log.info("Generating %s documentation..." % INFO['name'])
         docsrc = source_dir
         target = dest_dir
-        
+
         try:
             build = HtmlBuild()
             build.start({
@@ -65,11 +89,11 @@ def generate_docs():
                 'versioned': False
                 }, [])
             del build
-            
+
         except:
             log.error("The documentation generation failed.  Falling back to "
-                      "the zip file.")
-            
+                "the zip file.")
+
             # Unzip the docs into the 'html' folder.
             unzip_html_docs(html_zip, doc_dir)
     else:
@@ -78,8 +102,7 @@ def generate_docs():
         unzip_html_docs(html_zip, doc_dir)
 
 def unzip_html_docs(src_path, dest_dir):
-    """Given a path to a zipfile, extract
-    its contents to a given 'dest_dir'.
+    """ Given a path to a zipfile, extract its contents to a given 'dest_dir'.
     """
     file = zipfile.ZipFile(src_path)
     for name in file.namelist():
@@ -97,18 +120,34 @@ def unzip_html_docs(src_path, dest_dir):
 class my_develop(develop):
     def run(self):
         develop.run(self)
-        # Generate the documentation.
         generate_docs()
 
 class my_build(distbuild):
     def run(self):
         distbuild.run(self)
-        # Generate the documentation.
         generate_docs()
 
+
+# The actual setup call.
 setup(
-    author = 'Enthought, Inc',
-    author_email = 'info@enthought.com',
+    author = 'Peter Wang, et. al.',
+    author_email = 'pwang@enthought.com',
+    classifiers = """\
+        Development Status :: 4 - Beta
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: C
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines(),
     cmdclass = {
         'develop': my_develop,
         'build': my_build
@@ -116,12 +155,15 @@ setup(
     dependency_links = [
         'http://code.enthought.com/enstaller/eggs/source',
         ],
-    description = 'Chaco plotting toolkit',
+    description = DOCLINES[0],
     extras_require = INFO["extras_require"],
     ext_modules = [contour, speedups],
     include_package_data = True,
     install_requires = INFO["install_requires"],
     license = 'BSD',
+    long_description = '\n'.join(DOCLINES[2:]),
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
     name = INFO["name"],
     namespace_packages = [
         "enthought",
@@ -131,6 +173,7 @@ setup(
         'examples',
         'examples.zoomed_plot'
         ]),
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
     tests_require = [
         'nose >= 0.10.3',
         ],
@@ -139,3 +182,4 @@ setup(
     version = INFO["version"],
     zip_safe = False,
     )
+
