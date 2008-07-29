0d0392ecc734e518bb24f0d5e34fb9af830a3ca0
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-29 00:21:57 +0000

    ENH: [chaco] A warning is issued when importing anything under chaco2.

---
+++ enthought/chaco2/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn("""enthought.chaco2 has been renamed to enthought.chaco
+
+Please not that other objects have been renamed as well.
+For example Chaco2PlotItem has been renamed to ChacoPlotItem.""",
+              DeprecationWarning)
