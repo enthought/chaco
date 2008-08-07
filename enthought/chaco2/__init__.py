e9341654ae8308119bfc0c13bd0b1eaece5009ea
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-07 00:00:49 +0000

    improving deprecation comment in chaco2 __init__.py

--- enthought/chaco2/__init__.py
+++ enthought/chaco2/__init__.py
@@ -1,7 +1,4 @@
 import warnings
 
-warnings.warn("""enthought.chaco2 has been renamed to enthought.chaco
-
-Please not that other objects have been renamed as well.
-For example Chaco2PlotItem has been renamed to ChacoPlotItem.""",
+warnings.warn("""enthought.chaco2 has been renamed to enthought.chaco.  Please note that some objects have been renamed as well.  For example, Chaco2PlotItem has been renamed to ChacoPlotItem.""",
               DeprecationWarning)
