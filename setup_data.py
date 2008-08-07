f053666f333027678a1a92c9a16f3b1cb7b7fdc4
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-07 19:44:08 +0000

    * Synchronized dep versions with ETS 3.0.0b2 plans.

--- setup_data.py
+++ setup_data.py
@@ -18,8 +18,8 @@ def etsdep(p, min, max=None, literal=False):
 ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.0b2')
 ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0b2')
 #TRAITSBACKENDQT -- not needed due to the way it is used in chaco2_plot_container_editor.py
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.0')  # -- directly imported by chaco2_plot_editor.py
-TRAITS_UI = etsdep('Traits[ui]', '3.0.0')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.1b1')  # -- directly imported by chaco2_plot_editor.py
+TRAITS_UI = etsdep('Traits[ui]', '3.0.1b1')
 
 INFO = {
     "extras_require": {
