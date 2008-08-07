9b0275eaf5268b12f67f2e7e3c9027fbf9bf1331
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-07 20:00:50 +0000

    Fixed Numpy version info per discussion with Ilan.

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ Prerequisites
 You must have the following libraries installed before building or installing
 Chaco:
     
-* `Numpy <http://pypi.python.org/pypi/numpy/1.1.1>`_ version 1.0.4 or later. 
+* `Numpy <http://pypi.python.org/pypi/numpy/1.1.1>`_ version 1.1.0 or later is
+  preferred. Version 1.0.4 will work, but some tests may fail.
 * `setuptools <http://pypi.python.org/pypi/setuptools/0.6c8>`_
 
 """
