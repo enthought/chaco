6172c7139ad71c6127adfd57dcd2ff417b83e109
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-11 17:18:25 +0000

    Added the project to the packages' docstrings. This is obvious when looking at code, but not when looking at API docs.

---
+++ enthought/chaco/__init__.py
@@ -0,0 +1,9 @@
+#-----------------------------------------------------------------------------
+#
+#  Copyright (c) 2005 by Enthought, Inc.
+#  All rights reserved.
+#
+#-----------------------------------------------------------------------------
+""" Two-dimensionsal plotting application toolkit.
+    Part of the Chaco project of the Enthought Tool Suite.
+"""
