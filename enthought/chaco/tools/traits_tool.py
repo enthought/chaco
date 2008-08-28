6b07ae6f8e587fd27f9b30ec4922e255b428a794
kammeyer <kammeyer@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-28 03:20:03 +0000

    Brought TraitsTool up to date with current chaco architecture and added ability to specify custom classes to match and views for them

--- enthought/chaco/tools/traits_tool.py
+++ enthought/chaco/tools/traits_tool.py
@@ -2,10 +2,11 @@
 function.
 """
 # Enthought library imports
-from enthought.enable.api import BaseTool
+from enthought.enable.api import BaseTool, Container
+from enthought.traits.api import List, Dict
 
 # Chaco imports
-from enthought.chaco.api import BasePlotContainer, BaseXYPlot, PlotAxis
+from enthought.chaco.api import BasePlotContainer, BaseXYPlot, PlotAxis, ColorBar
 
 
 class Fifo(object):
@@ -33,7 +34,7 @@ class Fifo(object):
         return value
 
 
-def get_nested_components(container):
+def get_nested_components(container, classes):
     """ Returns a list of fundamental plotting components from a container
     with nested containers.  
     
@@ -46,11 +47,13 @@ def get_nested_components(container):
     worklist.append((container, (0,0)))
     while 1:
         item, offset = worklist.pop()
-        if isinstance(item, BasePlotContainer):
+        if isinstance(item, Container):
             new_offset = (offset[0]+item.x, offset[1]+item.y)
             for c in item.components:
                 worklist.append((c, new_offset))
-        elif isinstance(item, PlotAxis) or isinstance(item, BaseXYPlot):
+            for overlay in item.overlays + item.underlays:
+                components.append((overlay, offset))
+        elif any([isinstance(item, klass) for klass in classes]):
             components.append((item, offset))
             for overlay in item.overlays + item.underlays:
                 components.append((overlay, offset))
@@ -67,7 +70,12 @@ class TraitsTool(BaseTool):
     draw_mode = "none"
     # This tool is not visible (overrides BaseTool).
     visible = False
-    
+
+    # The classes of components that should trigger a traits view
+    classes = List([PlotAxis, ColorBar])
+
+    # A dict of Class : View providing alternate views for a particular component
+    views = Dict
     
     def normal_left_dclick(self, event):
         """ Handles the left mouse button being double-clicked when the tool
@@ -86,34 +94,28 @@ class TraitsTool(BaseTool):
         # then that is the only candidate.  If our component is a container,
         # then we add its non-container components to the list of candidates;
         # any nested containers are lower priority than primary plot components.
-        candidates = []
-        component = self.component
-        if isinstance(component, BasePlotContainer) or isinstance(component, BaseXYPlot):
-            candidates = get_nested_components(self.component)
-        elif isinstance(component, PlotAxis):
-            candidates = [(component, (0,0))]
-        else:
-            # We don't support clicking on unrecognized components
-            return
+        candidates = get_nested_components(self.component, [Container] + self.classes)
 
         # Hittest against all the candidate and take the first one
         item = None
         for candidate, offset in candidates:
-            if isinstance(candidate, PlotAxis):
-                if candidate.is_in(x-offset[0], y-offset[1]):
-                    item = candidate
-                    break
-            elif isinstance(candidate, BaseXYPlot):
-                if candidate.hittest((x-offset[0], y-offset[1])):
-                    item = candidate
-                    break
-
-        if item:
+            if candidate.is_in(x-offset[0], y-offset[1]):
+                item=candidate
+                break
+            
+        if item is not None:
             self.component.active_tool = self
-            item.edit_traits(kind="livemodal")
+            if item.__class__ in self.views:
+                item.edit_traits(kind="livemodal",
+                                 view=self.views[item.__class__],
+                                 parent=event.window.control)
+            else:
+                item.edit_traits(kind="livemodal",
+                                 parent=event.window.control)
             event.handled = True
             self.component.active_tool = None
             item.request_redraw()
+
         return
     
 
