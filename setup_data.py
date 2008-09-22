e9050ea979f7c096774438d307326fa8fc535254
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-22 21:27:28 +0000

    updating chaco's setup_data.py to work with ETS 3.0.2 versions of traits and EnthoughtBase

--- setup_data.py
+++ setup_data.py
@@ -16,10 +16,10 @@ def etsdep(p, min, max=None, literal=False):
 
 # Declare our ETS project dependencies.
 ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.2')
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.1')
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0')  # Chaco is compatible with ETS 3.0.2
 #TRAITSBACKENDQT -- not needed due to the way it is used in chaco2_plot_container_editor.py
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.3')  # -- directly imported by chaco2_plot_editor.py
-TRAITS_UI = etsdep('Traits[ui]', '3.0.3')
+#TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.2')  -- not needed due to the way it is used by chaco2_plot_editor.py
+TRAITS_UI = etsdep('Traits[ui]', '3.0.2')  # Chaco is compatible with ETS 3.0.2
 
 INFO = {
     "extras_require": {
