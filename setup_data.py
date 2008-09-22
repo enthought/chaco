744143540c1c678c1496b4e0f9385e64090e1413
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-22 21:34:56 +0000

    reverting removal of TRAITSBACKENDWX extras_require for chaco

--- setup_data.py
+++ setup_data.py
@@ -18,7 +18,7 @@ def etsdep(p, min, max=None, literal=False):
 ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.2')
 ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0')  # Chaco is compatible with ETS 3.0.2
 #TRAITSBACKENDQT -- not needed due to the way it is used in chaco2_plot_container_editor.py
-#TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.2')  -- not needed due to the way it is used by chaco2_plot_editor.py
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.2')  # used by chaco_plot_editor.py
 TRAITS_UI = etsdep('Traits[ui]', '3.0.2')  # Chaco is compatible with ETS 3.0.2
 
 INFO = {
