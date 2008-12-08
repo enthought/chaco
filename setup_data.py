61c9338033fe4e856220b743770127b4d65ccce1
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-12-08 23:59:22 +0000

    * Updated to require trunk for dependencies after discussion with Peter Wang.

--- setup_data.py
+++ setup_data.py
@@ -16,10 +16,10 @@ def etsdep(p, min, max=None, literal=False):
 
 # Declare our ETS project dependencies.
 ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.3')
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0')  # Chaco is compatible with ETS 3.0.2
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.2')
 #TRAITSBACKENDQT -- not needed due to the way it is used in chaco2_plot_container_editor.py
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.2')  # used by chaco_plot_editor.py
-TRAITS_UI = etsdep('Traits[ui]', '3.0.2')  # Chaco is compatible with ETS 3.0.2
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.4')  # used by chaco_plot_editor.py
+TRAITS_UI = etsdep('Traits[ui]', '3.0.4')
 
 INFO = {
     "extras_require": {
