08902f01b05fd2f4eb998a25f3e6091d4ce2d728
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-12-08 23:28:54 +0000

    * Minimum version bumps after ETS 3.1.0 release.

--- setup_data.py
+++ setup_data.py
@@ -15,7 +15,7 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.2')
+ENABLE_TRAITS = etsdep('Enable[traits]', '3.0.3')
 ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0')  # Chaco is compatible with ETS 3.0.2
 #TRAITSBACKENDQT -- not needed due to the way it is used in chaco2_plot_container_editor.py
 TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.2')  # used by chaco_plot_editor.py
@@ -41,5 +41,5 @@ INFO = {
         TRAITS_UI,
         ],
     "name": 'Chaco',
-    "version": '3.0.1',
+    "version": '3.0.2',
     }
