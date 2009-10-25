5b171fa3e7ccca600549f35c9e5f5d82a817535b
warren.weckesser <warren.weckesser@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-10-25 22:07:49 +0000

    Fixed the status overlay example (status_overlay.py). It was raising exceptions after a few button clicks.

--- examples/status_overlay.py
+++ examples/status_overlay.py
@@ -1,4 +1,6 @@
+
 import numpy
+
 from enthought.chaco.api import Plot, ArrayPlotData
 from enthought.chaco.layers.api import ErrorLayer, WarningLayer, StatusLayer
 from enthought.enable.component_editor import ComponentEditor
@@ -12,8 +14,8 @@ class MyPlot(HasTraits):
     plot = Instance(Plot)
     status_overlay = Instance(StatusLayer)
 
-    error_button = Button('error')
-    warn_button = Button('warning')
+    error_button = Button('Error')
+    warn_button = Button('Warning')
     no_problem_button = Button('No problem')
 
     traits_view = View( HGroup(Item('error_button', show_label=False),
@@ -34,9 +36,7 @@ class MyPlot(HasTraits):
         """ removes the old overlay and replaces it with
             an error overlay
         """
-        if self.status_overlay is not None:
-            # fade_out will remove the overlay when its done
-            self.status_overlay.fade_out()
+        self.clear_status()
 
         self.status_overlay = ErrorLayer(component=self.plot,
                                             align='ul', scale_factor=0.25)
@@ -48,9 +48,7 @@ class MyPlot(HasTraits):
         """ removes the old overlay and replaces it with
             an warning overlay
         """
-        if self.status_overlay is not None:
-            # fade_out will remove the overlay when its done
-            self.status_overlay.fade_out()
+        self.clear_status()
 
         self.status_overlay = WarningLayer(component=self.plot,
                                             align='ur', scale_factor=0.25)
@@ -61,12 +59,14 @@ class MyPlot(HasTraits):
     def _no_problem_button_fired(self, event):
         """ removes the old overlay
         """
-        if self.status_overlay is not None:
-            # fade_out will remove the overlay when its done
-            self.status_overlay.fade_out()
-
+        self.clear_status()
         self.plot.request_redraw()
 
+    def clear_status(self):
+        if self.status_overlay in self.plot.overlays:
+            # fade_out will remove the overlay when its done
+            self.status_overlay.fade_out()
+    
 index = numpy.array([1,2,3,4,5])
 data_series = index**2
 
