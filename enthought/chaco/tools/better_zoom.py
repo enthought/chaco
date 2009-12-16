5b13edb3ae493d9f339bf26264d7bd797a8bb50d
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-12-16 21:07:47 +0000

    ENH: better support for programatically zooming in by a given factor

--- enthought/chaco/tools/better_zoom.py
+++ enthought/chaco/tools/better_zoom.py
@@ -32,55 +32,39 @@ class BetterZoom(BaseTool, ToolHistoryMixin):
     max_zoom_out_factor = Float(1e5, allow_none=True)
 
     # The amount to zoom in by. The zoom out will be inversely proportional
-    zoom_in_increment = 2.0
+    zoom_factor = 2.0
 
     # The zoom factor on each axis
     _index_factor = Float(1.0)
     _value_factor = Float(1.0)
     
+    #--------------------------------------------------------------------------
+    #  public interface
+    #--------------------------------------------------------------------------
     
-    def zoom_in(self):
+    def zoom_in(self, factor=0):
+        if factor == 0:
+            factor = self.zoom_factor
         if self.axis != 'value':
-            self._zoom_in_mapper(self.component.index_mapper)
+            self._zoom_in_mapper(self.component.index_mapper, factor)
         if self.axis != 'index':
-            self._zoom_in_mapper(self.component.value_mapper)
+            self._zoom_in_mapper(self.component.value_mapper, factor)
             
         self.component.request_redraw()
     
-    def zoom_out(self):
+    def zoom_out(self, factor=0):
+        if factor == 0:
+            factor = self.zoom_factor
         if self.axis != 'value':
-            self._zoom_out_mapper(self.component.index_mapper)
+            self._zoom_out_mapper(self.component.index_mapper, factor)
         if self.axis != 'index':
-            self._zoom_out_mapper(self.component.value_mapper)
+            self._zoom_out_mapper(self.component.value_mapper, factor)
             
-        self.component.request_redraw()    
-    
-    def _zoom_in_mapper(self, mapper):
-        high = mapper.range.high
-        low = mapper.range.low
-        range = high-low
+        self.component.request_redraw() 
         
-        new_range = range/self.zoom_in_increment
-        mapper.range.high -= new_range/2
-        mapper.range.low += new_range/2
-
-    def _zoom_out_mapper(self, mapper):
-        high = mapper.range.high
-        low = mapper.range.low
-        range = high-low
-        
-        mapper.range.high += range/2
-        mapper.range.low -= range/2
-        
-    def _get_x_mapper(self):
-        if self.component.orientation == "h":
-            return self.component.index_mapper
-        return self.component.value_mapper
-
-    def _get_y_mapper(self):
-        if self.component.orientation == "h":
-            return self.component.value_mapper
-        return self.component.index_mapper
+    #--------------------------------------------------------------------------
+    #  BaseTool interface
+    #--------------------------------------------------------------------------
     
     def normal_key_pressed(self, event):
         """ Handles a key being pressed when the tool is in the 'normal'
@@ -93,16 +77,16 @@ class BetterZoom(BaseTool, ToolHistoryMixin):
             self.zoom_out()
             event.handled = True
         elif self.zoom_in_x_key.match(event):
-            self._zoom_in_mapper(self._get_x_mapper())
+            self._zoom_in_mapper(self._get_x_mapper(), self.zoom_factor)
             event.handled = True
         elif self.zoom_out_x_key.match(event):
-            self._zoom_out_mapper(self._get_x_mapper())
+            self._zoom_out_mapper(self._get_x_mapper(), self.zoom_factor)
             event.handled = True
         elif self.zoom_in_y_key.match(event):
-            self._zoom_in_mapper(self._get_y_mapper())
+            self._zoom_in_mapper(self._get_y_mapper(), self.zoom_factor)
             event.handled = True
         elif self.zoom_out_y_key.match(event):
-            self._zoom_out_mapper(self._get_y_mapper())
+            self._zoom_out_mapper(self._get_y_mapper(), self.zoom_factor)
             event.handled = True
         
         return
@@ -114,6 +98,41 @@ class BetterZoom(BaseTool, ToolHistoryMixin):
             else:
                 self.zoom_out()
             event.handled = True
+
+    #--------------------------------------------------------------------------
+    #  private interface
+    #--------------------------------------------------------------------------
+
+    def _zoom_in_mapper(self, mapper, factor):
+        high = mapper.range.high
+        low = mapper.range.low
+        range = high-low
+        center = numpy.mean((low, high))
+        
+        new_range = range/factor
+        mapper.range.high = center + new_range/2
+        mapper.range.low = center - new_range/2
+
+    def _zoom_out_mapper(self, mapper, factor):
+        high = mapper.range.high
+        low = mapper.range.low
+        range = high-low
+        center = numpy.mean((low, high))
+
+        new_range = range*factor
+        mapper.range.high = center + new_range/2
+        mapper.range.low = center - new_range/2
+        
+    def _get_x_mapper(self):
+        if self.component.orientation == "h":
+            return self.component.index_mapper
+        return self.component.value_mapper
+
+    def _get_y_mapper(self):
+        if self.component.orientation == "h":
+            return self.component.value_mapper
+        return self.component.index_mapper
+    
     
     #--------------------------------------------------------------------------
     #  ToolHistoryMixin interface
