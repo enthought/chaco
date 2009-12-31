130ca2aba2b9baaee71eb72cb03dd53e03975c7d
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-12-31 01:10:10 +0000

    making sure png-images get included

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@
 """
 Interactive 2-Dimensional Plotting
 
-Chaco is a Python plotting application toolkit that facilitates writing 
+Chaco is a Python plotting application toolkit that facilitates writing
 plotting applications at all levels of complexity, from simple scripts with
 hard-coded data to large plotting programs with complex data interrelationships
 and a multitude of interactive tools. While Chaco generates attractive static
 plots for publication and presentation, it also works well for interactive data
 visualization and exploration.
- 
+
 Features
 --------
 - **Flexible drawing and layout**: Plots consist of graphical components which
@@ -24,18 +24,18 @@ Features
 - **Modular and extensible architecture**: Chaco is object-oriented from the
   ground up for ease of extension and customization. There are clear interfaces
   and abstract classes defining extension points for writing your own custom
-  behaviors, from custom tools, plot types, layouts, etc. Most classes are 
+  behaviors, from custom tools, plot types, layouts, etc. Most classes are
   also "subclass-friendly", so that subclasses can override one or two methods
   and everything else just works.
 - **Data model for ease of extension and embedding**: Chaco separates the data
   from any transformations of the data that are needed for displaying it. This
   separation makes it easier to extend Chaco, or embed it in applications.
-  
+
 Prerequisites
 -------------
 You must have the following libraries installed before building or installing
 Chaco:
-    
+
 * `Numpy <http://pypi.python.org/pypi/numpy/1.1.1>`_ version 1.1.0 or later is
   preferred. Version 1.0.4 will work, but some tests may fail.
 * `setuptools <http://pypi.python.org/pypi/setuptools/0.6c8>`_
@@ -126,9 +126,10 @@ setup(
         'build': MyBuild
     },
     data_files=[('enthought/chaco/layers/data',
-        ['enthought/chaco/layers/data/Dialog-error.svg', 
-         'enthought/chaco/layers/data/Dialog-warning.svg', 
+        ['enthought/chaco/layers/data/Dialog-error.svg',
+         'enthought/chaco/layers/data/Dialog-warning.svg',
          'enthought/chaco/layers/data/range_selection.svg'])],
+    package_data={'enthought': ['chaco/tools/toolbars/images/*.png']},
     description = DOCLINES[1],
     download_url = ('http://www.enthought.com/repo/ETS/Chaco-%s.tar.gz' %
         INFO['version']),
