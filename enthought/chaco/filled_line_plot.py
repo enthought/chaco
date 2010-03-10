033ee05bfc8131881d9f74cf05a2cf6791d0cdd2
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-03-10 18:31:58 +0000

    ENH - wrapped gc operations in try/finally which restores state. I haven't found this to be necessary, but it creates safer code

--- enthought/chaco/filled_line_plot.py
+++ enthought/chaco/filled_line_plot.py
@@ -27,46 +27,48 @@ class FilledLinePlot(PolygonPlot):
             ox, oy = self.map_screen([[self.x_mapper.range.high, 
                                       self.y_mapper.range.high]])[0]
         gc.save_state()
-
-        gc.clip_to_rect(self.x, self.y, self.width, self.height)
-
-        # If the fill color is not transparent, then draw the fill polygon first
-        if self.face_color_[-1] != 0 and self.face_color_[:3] != (0,0,0):
-            gc.set_fill_color(self.face_color_)
-            gc.begin_path()
-            startx, starty = points[0]
-            if self.orientation == "h":
-                gc.move_to(startx, oy)
-                gc.line_to(startx, starty)
-            else:
-                gc.move_to(ox, starty)
-                gc.line_to(startx, starty)
-            
-            gc.lines(points)
-
-            endx, endy = points[-1]
-            if self.orientation == "h":
-                gc.line_to(endx, oy)
-                gc.line_to(startx, oy)
-            else:
-                gc.line_to(ox, endy)
-                gc.line_to(ox, starty)
-
-            gc.close_path()
-            gc.fill_path()
-
-        # If the line color is not transparent, or tha same color
-        # as the filled area:
-        if self.edge_color_[-1] != 0 and self.edge_color_ != self.face_color_:
-            gc.set_stroke_color(self.edge_color_)
-            gc.set_line_width(self.edge_width)
-            gc.set_line_dash(self.edge_style_)
-            gc.begin_path()
-            gc.move_to(*points[0])
-            gc.lines(points)
-            gc.stroke_path()
-
-        gc.restore_state()
+        
+        try:
+            gc.clip_to_rect(self.x, self.y, self.width, self.height)
+    
+            # If the fill color is not transparent, then draw the fill polygon first
+            if self.face_color_[-1] != 0 and self.face_color_[:3] != (0,0,0):
+                gc.set_fill_color(self.face_color_)
+                gc.begin_path()
+                startx, starty = points[0]
+                if self.orientation == "h":
+                    gc.move_to(startx, oy)
+                    gc.line_to(startx, starty)
+                else:
+                    gc.move_to(ox, starty)
+                    gc.line_to(startx, starty)
+                
+                gc.lines(points)
+    
+                endx, endy = points[-1]
+                if self.orientation == "h":
+                    gc.line_to(endx, oy)
+                    gc.line_to(startx, oy)
+                else:
+                    gc.line_to(ox, endy)
+                    gc.line_to(ox, starty)
+    
+                gc.close_path()
+                gc.fill_path()
+    
+            # If the line color is not transparent, or tha same color
+            # as the filled area:
+            if self.edge_color_[-1] != 0 and self.edge_color_ != self.face_color_:
+                gc.set_stroke_color(self.edge_color_)
+                gc.set_line_width(self.edge_width)
+                gc.set_line_dash(self.edge_style_)
+                gc.begin_path()
+                gc.move_to(*points[0])
+                gc.lines(points)
+                gc.stroke_path()
+                
+        finally:
+            gc.restore_state()
 
 
 
