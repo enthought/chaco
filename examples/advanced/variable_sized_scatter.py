2e41734cfef3da2c76dfcc4d4afe108d362fe735
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2011-01-27 17:47:40 +0000

    cleanup

--- examples/advanced/variable_sized_scatter.py
+++ examples/advanced/variable_sized_scatter.py
@@ -1,11 +1,11 @@
 """
 Draws a scatterplot of a set of random points of variable size.
- - This uses the non-standard renderer, VariableSizeScatterPlot 
+ - This uses the non-standard renderer, VariableSizeScatterPlot
  - Left-drag pans the plot.
  - Mousewheel up and down zooms the plot in and out.
- - Pressing "z" brings up the Zoom Box, and you can click-drag a rectangular 
+ - Pressing "z" brings up the Zoom Box, and you can click-drag a rectangular
    region to zoom.  If you use a sequence of zoom boxes, pressing alt-left-arrow
-   and alt-right-arrow moves you forwards and backwards through the "zoom 
+   and alt-right-arrow moves you forwards and backwards through the "zoom
    history".
 """
 
@@ -35,16 +35,16 @@ def _create_plot_component():
     x = numpy.arange(0, numpts)
     y = numpy.random.random(numpts)
     marker_size = numpy.random.normal(4.0, 4.0, numpts)
-    
+
     # Create a plot data object and give it this data
     pd = ArrayPlotData()
     pd.set_data("index", x)
     pd.set_data("value", y)
-    
+
     # Because this is a non-standard renderer, we can't call plot.plot, which
     # sets up the array data sources, mappers and default index/value ranges.
     # So, its gotta be done manually for now.
-    
+
     index_ds = ArrayDataSource(x)
     value_ds = ArrayDataSource(y)
 
@@ -52,8 +52,8 @@ def _create_plot_component():
     plot = Plot(pd)
     plot.index_range.add(index_ds)
     plot.value_range.add(value_ds)
-    
-    # Create the index and value mappers using the plot data ranges 
+
+    # Create the index and value mappers using the plot data ranges
     imapper = LinearMapper(range=plot.index_range)
     vmapper = LinearMapper(range=plot.value_range)
 
@@ -66,7 +66,7 @@ def _create_plot_component():
                     marker='circle',
                     marker_size=marker_size,
                     color=(1.0,0.0,0.75,0.4))
-    
+
     # Append the renderer to the list of the plot's plots
     plot.add(scatter)
     plot.plots['var_size_scatter'] = [scatter]
@@ -80,7 +80,7 @@ def _create_plot_component():
     plot.tools.append(PanTool(plot, constrain_key="shift"))
     zoom = ZoomTool(component=plot, tool_mode="box", always_on=False)
     plot.overlays.append(zoom)
-    
+
     return plot
 
 #===============================================================================
@@ -88,25 +88,25 @@ def _create_plot_component():
 size = (650, 650)
 title = "Basic scatter plot"
 bg_color="lightgray"
-        
+
 #===============================================================================
 # # Demo class that is used by the demo.py application.
 #===============================================================================
 class Demo(HasTraits):
     plot = Instance(Component)
-    
+
     traits_view = View(
                     Group(
                         Item('plot', editor=ComponentEditor(size=size,
-                                                            bgcolor=bg_color), 
+                                                            bgcolor=bg_color),
                              show_label=False),
                         orientation = "vertical"),
                     resizable=True, title=title
                     )
-    
+
     def _plot_default(self):
          return _create_plot_component()
-    
+
 demo = Demo()
 
 #===============================================================================
@@ -118,7 +118,7 @@ class PlotFrame(DemoFrame):
         # Return a window containing our plots
         return Window(self, -1, component=_create_plot_component(),
                       bg_color=bg_color)
-    
+
 if __name__ == "__main__":
     demo_main(PlotFrame, size=size, title=title)
 
