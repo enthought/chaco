b5fdda040b90b5f0c1d22c83547bcbe8153297fd
brycehendrix <brycehendrix@gmail.com>
2011-03-11 12:06:58 -0600

    added another origin/orientation example

--- examples/depth.py
+++ examples/depth.py
@@ -5,22 +5,23 @@ from enthought.traits.api import HasTraits, Instance
 from enthought.traits.ui.api import Item, View
 
 class MyPlot(HasTraits):
-    """ Plot with the origin in the upper left instead of lower left
+    """ Plot where depth is the index such that the plot is vertical
+        and the origin is the upper left
     """
     plot = Instance(Plot)
 
     traits_view = View(Item('plot', editor=ComponentEditor()))
 
-    def __init__(self, index, data_series, **kw):
+    def __init__(self, depth, data_series, **kw):
         super(MyPlot, self).__init__(**kw)
 
-        plot_data = ArrayPlotData(index=index)
+        plot_data = ArrayPlotData(index=depth)
         plot_data.set_data('data_series', data_series)
-        self.plot = Plot(plot_data, origin='top left')
+        self.plot = Plot(plot_data, orientation='v', origin='top left')
         self.plot.plot(('index', 'data_series'))
         
-index = numpy.array([1,2,3,4,5])
-data_series = index**2
+depth = numpy.array([1,2,3,4,5])
+data_series = depth**2
 
-my_plot = MyPlot(index, data_series)
+my_plot = MyPlot(depth, data_series)
 my_plot.configure_traits()
