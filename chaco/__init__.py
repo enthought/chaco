be72f4cab0df24db6cb0adf7ddea3a1526a92fbc
Ilan Schnell <ischnell@enthought.com>
2011-04-23 09:49:43 -0500

    adding version to __init__.py file

--- chaco/__init__.py
+++ chaco/__init__.py
@@ -1,9 +1,6 @@
-#-----------------------------------------------------------------------------
-#
-#  Copyright (c) 2005 by Enthought, Inc.
+#  Copyright (c) 2005-2011 by Enthought, Inc.
 #  All rights reserved.
-#
-#-----------------------------------------------------------------------------
 """ Two-dimensionsal plotting application toolkit.
     Part of the Chaco project of the Enthought Tool Suite.
 """
+__version__ = '3.4.1'
