4e55341c2a8c01b255e90e19ae651e7bdb4464ef
Andy R. Terrel <aterrel@enthought.com>
2011-05-23 13:04:29 -0500

    Organize simple_line.py to new style adjust to PEP 8

--- examples/simple_line.py
+++ examples/simple_line.py
@@ -7,9 +7,9 @@ Right-drag (in the Y direction) zooms the plot in and out.
 
 Mousewheel up and down zooms the plot in and out.
 
-Pressing "z" brings up the Zoom Box, and you can click-drag a rectangular region to
-zoom.  If you use a sequence of zoom boxes, pressing alt-left-arrow and
-alt-right-arrow moves you forwards and backwards through the "zoom history".
+Pressing "z" brings up the Zoom Box, and you can click-drag a rectangular
+region to zoom.  If you use a sequence of zoom boxes, pressing alt-left-arrow
+and alt-right-arrow moves you forwards and backwards through the "zoom history".
 
 Right-click and dragging on the legend allows you to reposition the legend.
 
@@ -20,12 +20,9 @@ Double-clicking on line or scatter plots brings up a traits editor for the plot.
 from numpy import arange
 from scipy.special import jn
 
-from enthought.chaco.example_support import COLOR_PALETTE
-from enthought.enable.example_support import DemoFrame, demo_main
-
 # Enthought library imports
-from enthought.enable.api import Window, Component, ComponentEditor
-from enthought.traits.api import HasTraits, Instance
+from enthought.enable.api import Component, ComponentEditor
+from enthought.traits.api import Float, HasTraits, Int, Instance
 from enthought.traits.ui.api import Item, Group, View
 
 # Chaco imports
@@ -34,118 +31,122 @@ from enthought.chaco.api import create_line_plot, add_default_axes, \
         create_scatter_plot, Legend
 from enthought.chaco.tools.api import PanTool, ZoomTool, LegendTool, \
         TraitsTool, DragZoom
+from enthought.chaco.example_support import COLOR_PALETTE
 
-#===============================================================================
-# # Create the Chaco plot.
-#===============================================================================
-def _create_plot_component():
-    container = OverlayPlotContainer(padding = 50, fill_padding = True,
-                                     bgcolor = "lightgray", use_backbuffer=True)
 
-    # Create the initial X-series of data
-    numpoints = 100
-    low = -5
-    high = 15.0
-    x = arange(low, high+0.001, (high-low)/numpoints)
+class OverlappingPlotContainer(OverlayPlotContainer):
+    """Simple container for creating a series of plots"""
 
-    # Plot some bessel functions
-    value_mapper = None
-    index_mapper = None
-    plots = {}
-    for i in range(10):
-        y = jn(i, x)
-        if i%2 == 1:
-            plot = create_line_plot((x,y), color=tuple(COLOR_PALETTE[i]), width=2.0)
-            plot.index.sort_order = "ascending"
-        else:
-            plot = create_scatter_plot((x,y), color=tuple(COLOR_PALETTE[i]))
+    numpoints = Int(100)
+    low = Float(-5.0)
+    high = Float(15.0)
+    num_funs = Int(10)
 
+    def __init__(self, *args, **kws):
+        super(OverlayPlotContainer, self).__init__(*args, **kws)
+        self._setup_plots()
+
+    def _setup_plots(self):
+        """Creates series of Bessel function plots"""
+        plots = {}
+        x = arange(self.low, self.high + 0.001,
+                   (self.high - self.low) / self.numpoints)
+
+        for i in range(self.num_funs):
+            y = jn(i, x)
+            if i % 2 == 1:
+                plot = create_line_plot((x, y),
+                                        color=tuple(COLOR_PALETTE[i]),
+                                        width=2.0)
+            else:
+                plot = create_scatter_plot((x, y),
+                                            color=tuple(COLOR_PALETTE[i]))
+
+            if i == 0:
+                value_mapper, index_mapper, legend = \
+                    self._setup_plot_tools(plot)
+            else:
+                self._setup_mapper(plot, value_mapper, index_mapper)
+
+            self.add(plot)
+            plots["Bessel j_%d" % i] = plot
+
+        # Set the list of plots on the legend
+        legend.plots = plots
+
+        # Add the title at the top
+        self.overlays.append(PlotLabel("Bessel functions",
+                                       component=self,
+                                       font="swiss 16",
+                                       overlay_position="top"))
+
+        # Add the traits inspector tool to the container
+        self.tools.append(TraitsTool(self))
+
+    def _setup_plot_tools(self, plot):
+        """Sets up the background, and several tools on a plot"""
+        # Make a white background with grids and axes
         plot.bgcolor = "white"
-        plot.border_visible = True
-        if i != 0:
-            plot.value_mapper = value_mapper
-            value_mapper.range.add(plot.value)
-            plot.index_mapper = index_mapper
-            index_mapper.range.add(plot.index)
+        add_default_grids(plot)
+        add_default_axes(plot)
 
-        else:
-            value_mapper = plot.value_mapper
-            index_mapper = plot.index_mapper
-            add_default_grids(plot)
-            add_default_axes(plot)
-            plot.index_range.tight_bounds = False
-            plot.index_range.refresh()
-            plot.value_range.tight_bounds = False
-            plot.value_range.refresh()
-            plot.tools.append(PanTool(plot))
+        # Allow white space around plot
+        plot.index_range.tight_bounds = False
+        plot.index_range.refresh()
+        plot.value_range.tight_bounds = False
+        plot.value_range.refresh()
 
-            # The ZoomTool tool is stateful and allows drawing a zoom
-            # box to select a zoom region.
-            zoom = ZoomTool(plot, tool_mode="box", always_on=False)
-            plot.overlays.append(zoom)
+        # The PanTool allows panning around the plot
+        plot.tools.append(PanTool(plot))
 
-            # The DragZoom tool just zooms in and out as the user drags
-            # the mouse vertically.
-            dragzoom = DragZoom(plot, drag_button="right")
-            plot.tools.append(dragzoom)
+        # The ZoomTool tool is stateful and allows drawing a zoom
+        # box to select a zoom region.
+        zoom = ZoomTool(plot, tool_mode="box", always_on=False)
+        plot.overlays.append(zoom)
 
-            # Add a legend in the upper right corner, and make it relocatable
-            legend = Legend(component=plot, padding=10, align="ur")
-            legend.tools.append(LegendTool(legend, drag_button="right"))
-            plot.overlays.append(legend)
+        # The DragZoom tool just zooms in and out as the user drags
+        # the mouse vertically.
+        dragzoom = DragZoom(plot, drag_button="right")
+        plot.tools.append(dragzoom)
 
-        container.add(plot)
-        plots["Bessel j_%d"%i] = plot
+        # Add a legend in the upper right corner, and make it relocatable
+        legend = Legend(component=plot, padding=10, align="ur")
+        legend.tools.append(LegendTool(legend, drag_button="right"))
+        plot.overlays.append(legend)
 
-    # Set the list of plots on the legend
-    legend.plots = plots
+        return plot.value_mapper, plot.index_mapper, legend
 
-    # Add the title at the top
-    container.overlays.append(PlotLabel("Bessel functions",
-                              component=container,
-                              font = "swiss 16",
-                              overlay_position="top"))
+    def _setup_mapper(self, plot, value_mapper, index_mapper):
+        """Sets up a mapper for given plot"""
+        plot.value_mapper = value_mapper
+        value_mapper.range.add(plot.value)
+        plot.index_mapper = index_mapper
+        index_mapper.range.add(plot.index)
 
-    # Add the traits inspector tool to the container
-    container.tools.append(TraitsTool(container))
 
-    return container
+size = (800, 700)
+title = "Simple Line Plot"
 
-#===============================================================================
-# Attributes to use for the plot view.
-size=(800,700)
-title="Simple Line Plot"
 
-#===============================================================================
-# # PlotExamplen class that is used by the demo.py application.
-#===============================================================================
-class PlotExamplen(HasTraits):
+class PlotExample(HasTraits):
     plot = Instance(Component)
 
     traits_view = View(
                     Group(
                         Item('plot', editor=ComponentEditor(size=size),
                              show_label=False),
-                        orientation = "vertical"),
+                        orientation="vertical"),
                     resizable=True, title=title,
                     width=size[0], height=size[1]
                     )
 
     def _plot_default(self):
-         return _create_plot_component()
+        return OverlappingPlotContainer(padding=50, fill_padding=True,
+                                     bgcolor="lightgray", use_backbuffer=True)
 
-demo = PlotExamplen()
 
-#===============================================================================
-# Stand-alone frame to display the plot.
-#===============================================================================
-class PlotFrame(DemoFrame):
+demo = PlotExample()
 
-    def _create_window(self):
-        # Return a window containing our plots
-        return Window(self, -1, component=_create_plot_component())
 
 if __name__ == "__main__":
-    demo_main(PlotFrame, size=size, title=title)
-
-# EOF
+    demo.configure_traits()
