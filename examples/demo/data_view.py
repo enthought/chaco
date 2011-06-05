5f12e03070025847866384d5d043a6e37fd9b205
Andy R. Terrel <aterrel@enthought.com>
2011-06-05 16:37:22 -0500

    Updating data_view.py

--- examples/demo/data_view.py
+++ examples/demo/data_view.py
@@ -8,83 +8,54 @@ from numpy import linspace, sin, cos
 from enthought.chaco.api import DataView, ArrayDataSource, ScatterPlot, \
                                 LinePlot, LinearMapper
 from enthought.chaco.tools.api import PanTool, ZoomTool
-from enthought.enable.example_support import DemoFrame, demo_main
-from enthought.enable.api import Component, ComponentEditor, Window
+from enthought.enable.api import Component, ComponentEditor
 from enthought.traits.api import HasTraits, Instance
-from enthought.traits.ui.api import Item, Group, View
+from enthought.traits.ui.api import Item, View
 
-#===============================================================================
-# # Create the Chaco plot.
-#===============================================================================
-def _create_plot_component():
-    x = linspace(-5, 10, 500)
-    y = sin(x)
-    y2 = 0.5 * cos(2*x)
-
-    view = DataView(border_visible = True)
-    scatter = ScatterPlot(index = ArrayDataSource(x),
-                          value = ArrayDataSource(y),
-                          marker = "square",
-                          color = "red",
-                          outline_color = "transparent",
-                          index_mapper = LinearMapper(range=view.index_range),
-                          value_mapper = LinearMapper(range=view.value_range))
-
-    line = LinePlot(index = scatter.index,
-                    value = ArrayDataSource(y2),
-                    color = "blue",
-                    index_mapper = LinearMapper(range=view.index_range),
-                    value_mapper = LinearMapper(range=view.value_range))
-
-    # Add the plot's index and value datasources to the dataview's
-    # ranges so that it can auto-scale and fit appropriately
-    view.index_range.sources.append(scatter.index)
-    view.value_range.sources.append(scatter.value)
-    view.value_range.sources.append(line.value)
-
-    # Add the renderers to the dataview.  The z-order is determined
-    # by the order in which renderers are added.
-    view.add(scatter)
-    view.add(line)
-    view.tools.append(PanTool(view))
-    view.overlays.append(ZoomTool(view))
-
-    return view
-
-#===============================================================================
-# Attributes to use for the plot view.
-size=(800,700)
-title="Dataview + renderer example"
-
-#===============================================================================
-# # Demo class that is used by the demo.py application.
-#===============================================================================
-class Demo(HasTraits):
+class PlotExample(HasTraits):
     plot = Instance(Component)
 
-    traits_view = View(
-                    Group(
-                        Item('plot', editor=ComponentEditor(size=size),
-                             show_label=False),
-                        orientation = "vertical"),
-                    resizable=True, title=title
-                    )
+    traits_view = View(Item('plot', editor=ComponentEditor(size=(800, 700))),
+                       resizable=True, title="Dataview + renderer example"
+                       )
 
     def _plot_default(self):
-         return _create_plot_component()
+        x = linspace(-5, 10, 500)
+        y = sin(x)
+        y2 = 0.5 * cos(2*x)
 
-demo = Demo()
+        view = DataView(border_visible = True)
+        scatter = ScatterPlot(index = ArrayDataSource(x),
+                              value = ArrayDataSource(y),
+                              marker = "square",
+                              color = "red",
+                              outline_color = "transparent",
+                              index_mapper = LinearMapper(range=view.index_range),
+                              value_mapper = LinearMapper(range=view.value_range))
 
-#===============================================================================
-# Stand-alone frame to display the plot.
-#===============================================================================
-class PlotFrame(DemoFrame):
+        line = LinePlot(index = scatter.index,
+                        value = ArrayDataSource(y2),
+                        color = "blue",
+                        index_mapper = LinearMapper(range=view.index_range),
+                        value_mapper = LinearMapper(range=view.value_range))
 
-    def _create_window(self):
-        # Return a window containing our plots
-        return Window(self, -1, component=_create_plot_component())
+        # Add the plot's index and value datasources to the dataview's
+        # ranges so that it can auto-scale and fit appropriately
+        view.index_range.sources.append(scatter.index)
+        view.value_range.sources.append(scatter.value)
+        view.value_range.sources.append(line.value)
+
+        # Add the renderers to the dataview.  The z-order is determined
+        # by the order in which renderers are added.
+        view.add(scatter)
+        view.add(line)
+        view.tools.append(PanTool(view))
+        view.overlays.append(ZoomTool(view))
+
+        return view
+
+
+demo = PlotExample()
 
 if __name__ == "__main__":
-    demo_main(PlotFrame, size=size, title=title)
-
-#--EOF---
+    demo.configure_traits()
