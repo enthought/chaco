aad4465e4ecdb906dce066e9f0eb75c22285764f
Andy R. Terrel <aterrel@enthought.com>
2011-06-05 17:30:25 -0500

    Updating functionplotter.py

--- examples/demo/functionplotter.py
+++ examples/demo/functionplotter.py
@@ -5,119 +5,87 @@ and returns different data depending on that range.
 """
 
 # Major library imports
-from numpy import array, linspace, sin, ceil
-from scipy.special import jn
-
-from enthought.enable.example_support import DemoFrame, demo_main
+from numpy import linspace, sin, ceil
 
 # Enthought library imports
-from enthought.enable.api import Window, Component, ComponentEditor
-from enthought.traits.api import HasTraits, Instance, Callable, on_trait_change
+from enthought.enable.api import Component, ComponentEditor
+from enthought.traits.api import HasTraits, Instance, Int
 from enthought.traits.ui.api import Item, Group, HGroup, View
 
 # Chaco imports
-from enthought.chaco.api import ArrayPlotData, HPlotContainer, Plot, \
-        LinearMapper, ScatterPlot, DataView, LinePlot
+from enthought.chaco.api import ScatterPlot, DataView, LinePlot
 from enthought.chaco.tools.api import PanTool, ZoomTool
 from enthought.chaco.function_data_source import FunctionDataSource
 
-#===============================================================================
-# # Create the Chaco plot.
-#===============================================================================
-numpoints = 500
 
-def xfunc(low, high):
-    dx = (high - low) / numpoints
-    real_low = ceil(low/dx) * dx
-    real_high = ceil(high/dx) * dx
-    return linspace(real_low, real_high, numpoints)
-
-def yfunc(low, high):
-    x = xfunc(low, high)
-    #return jn(0, x)
-    return sin(1.0/x)
-
-def _create_plot_component():
-
-    container = DataView()
-
-    xds = FunctionDataSource(func = xfunc)
-    yds = FunctionDataSource(func = yfunc)
-
-    xmapper = container.x_mapper
-    ymapper = container.y_mapper
-
-    xds.data_range = xmapper.range
-    yds.data_range = xmapper.range
-
-    xmapper.range.set_bounds(-5, 10)
-    ymapper.range.set_bounds(-1, 1.2)
-
-    plot = ScatterPlot(index = xds, value = yds, index_mapper = xmapper,
-                       value_mapper = ymapper,
-                       color = "green",
-                       marker = "circle",
-                       marker_size = 3,
-                       line_width = 0)
-
-    plot2 = LinePlot(index = xds, value = yds, index_mapper = xmapper,
-                    value_mapper = ymapper,
-                    color = "lightgray")
-
-    container.add(plot2, plot)
-    plot.tools.append(PanTool(plot, constrain_direction="x", constrain=True))
-    plot.tools.append(ZoomTool(plot, axis="index", tool_mode="range"))
-
-    return container
-
-#===============================================================================
-# Attributes to use for the plot view.
-size=(900,500)
-title="Function Plot"
-
-#===============================================================================
-# # Demo class that is used by the demo.py application.
-#===============================================================================
-class Demo(HasTraits):
+class PlotExample(HasTraits):
     plot = Instance(Component)
+    numpoints = Int(500)
 
     traits_view = View(
-                    Group(
-                        Item('plot', editor=ComponentEditor(size=size),
-                             show_label=False),
-                        HGroup(
-                            Item('object.plot.x_mapper.range.high_setting',
-                                label='High'),
-                            Item('object.plot.x_mapper.range.low_setting',
-                                 label='Low'),
-                                 label='X', show_border=True),
-                        HGroup(
-                            Item('object.plot.y_mapper.range.high_setting',
-                                 label='High'),
-                            Item('object.plot.y_mapper.range.low_setting',
-                                 label='Low'),
-                                 label='Y', show_border=True),
-                        orientation = "vertical"),
-                    resizable=True, title=title,
-                    width=size[0], height=size[1]
-                    )
+        Group(
+            Item('plot', editor=ComponentEditor(), show_label=False),
+            HGroup(
+                HGroup(
+                    Item('object.plot.x_mapper.range.low_setting', label='Low'),
+                    Item('object.plot.x_mapper.range.high_setting', label='High'),
+                    label='X', show_border=True
+                ),
+                HGroup(
+                    Item('object.plot.y_mapper.range.low_setting', label='Low'),
+                    Item('object.plot.y_mapper.range.high_setting', label='High'),
+                    label='Y', show_border=True
+                ),
+            ),
+            orientation = "vertical"), resizable=True, title="Function Plot",
+            width=900, height=600
+        )
+
+    def xfunc(self, low, high):
+        dx = (high - low) / self.numpoints
+        real_low = ceil(low/dx) * dx
+        real_high = ceil(high/dx) * dx
+        return linspace(real_low, real_high, self.numpoints)
+
+    def yfunc(self, low, high):
+        x = self.xfunc(low, high)
+        return sin(1.0/x)
 
     def _plot_default(self):
-         return _create_plot_component()
+        container = DataView()
 
-demo = Demo()
+        xds = FunctionDataSource(func = self.xfunc)
+        yds = FunctionDataSource(func = self.yfunc)
 
-#===============================================================================
-# Stand-alone frame to display the plot.
-#===============================================================================
-class PlotFrame(DemoFrame):
+        xmapper = container.x_mapper
+        ymapper = container.y_mapper
 
-    def _create_window(self):
-        # Return a window containing our plots
-        return Window(self, -1, component=_create_plot_component())
+        xds.data_range = xmapper.range
+        yds.data_range = xmapper.range
+
+        xmapper.range.set_bounds(-5, 10)
+        ymapper.range.set_bounds(-1, 1.2)
+
+        plot = ScatterPlot(index = xds, value = yds, index_mapper = xmapper,
+                           value_mapper = ymapper,
+                           color = "green",
+                           marker = "circle",
+                           marker_size = 3,
+                           line_width = 0)
+
+        plot2 = LinePlot(index = xds, value = yds, index_mapper = xmapper,
+                        value_mapper = ymapper,
+                        color = "lightgray")
+
+        container.add(plot2, plot)
+        plot.tools.append(PanTool(plot, constrain_direction="x", constrain=True))
+        plot.tools.append(ZoomTool(plot, axis="index", tool_mode="range"))
+
+        return container
+
+
+demo = PlotExample()
 
 if __name__ == "__main__":
-    demo_main(PlotFrame, size=size, title=title)
-
-#--EOF---
+    demo.configure_traits()
 
