3f3ce2614855177b5dbfe32bcab3fb9e5bdc06f6
Ilan Schnell <ischnell@enthought.com>
2011-06-15 22:29:37 -0500

    DOC: add correct version to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,16 +35,14 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General substitutions.
-project = 'Chaco'
-copyright = '2008, Enthought, Inc.'
+project = 'chaco'
+copyright = '2008-2011, Enthought, Inc.'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
-#
-# Pull from the actual release number in setup_data
-sys.path.insert(0, os.path.join('..', '..'))
-from setup_data import INFO
-version = release = INFO['version']
+d = {}
+execfile(os.path.join('..', '..', 'chaco', '__init__.py'), d)
+version = release = d['__version__']
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
