af60564352462ebcc51db2a01fe9a4f11da4fabb
Warren Weckesser <warren.weckesser@enthought.com>
2011-09-01 19:35:46 -0500

    DOC: Fix typo in docstring.

--- chaco/__init__.py
+++ chaco/__init__.py
@@ -1,6 +1,6 @@
 #  Copyright (c) 2005-2011 by Enthought, Inc.
 #  All rights reserved.
-""" Two-dimensionsal plotting application toolkit.
+""" Two-dimensional plotting application toolkit.
     Part of the Chaco project of the Enthought Tool Suite.
 """
 __version__ = '4.1.0'
