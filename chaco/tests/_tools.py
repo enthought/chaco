aeb05f457213c6b240c84816599ceb4c9d8e10b8
Pietro Berkes <pietro.berkes@googlemail.com>
2011-11-28 17:16:32 +0000

    FIX: fix bounds bug reported in 875b4d6938c12a5894c5d194ae5a8f6fde23aeb9
    The functionality that transforms bounds into linspaces has been
    exctracted in a new method in the Plot class.

---
+++ chaco/tests/_tools.py
@@ -0,0 +1,40 @@
+from contextlib import contextmanager
+
+import sys
+import traceback
+
+
+# ######### Testing tools
+
+@contextmanager
+def store_exceptions_on_all_threads():
+    """Context manager that captures all exceptions, even those coming from
+    the UI thread. On exit, the first exception is raised (if any).
+    """
+
+    exceptions = []
+
+    def excepthook(type, value, tb):
+        exceptions.append(value)
+        message = 'Uncaught exception:\n'
+        message += ''.join(traceback.format_exception(type, value, tb))
+        print message
+
+    try:
+        sys.excepthook = excepthook
+        yield
+    finally:
+        if len(exceptions) > 0:
+            raise exceptions[0]
+        sys.excepthook = sys.__excepthook__
+
+
+@contextmanager
+def assert_raises(ExceptionClass):
+    try:
+        yield
+    except ExceptionClass:
+        pass
+    else:
+        msg = 'Test should have failed with {}.'
+        raise Exception(msg.format(ExceptionClass.__name__))
--- chaco/tests/create_2d_test_case.py
+++ chaco/tests/create_2d_test_case.py
@@ -6,6 +6,9 @@ from traitsui.api import Item, View
 
 import numpy as np
 
+import nose
+from _tools import store_exceptions_on_all_threads, assert_raises
+
 
 class PlotViewer(HasTraits):
     plot = Instance(Plot)
@@ -28,10 +31,62 @@ def test_bounds_2d_case():
     plot.contour_plot("z", xbounds=x, ybounds=y)
 
     # try to display it, that's when the exception is raised
-    pv = PlotViewer(plot=plot)
-    pv.edit_traits()
+    with store_exceptions_on_all_threads():
+        pv = PlotViewer(plot=plot)
+        pv.edit_traits()
+
+
+def test_process_2d_bounds():
+    # behavior: _process_2d_bounds accepts all possible ways to set x and y
+    # bounds in 2d plots and returns a 1d array with equally spaced
+    # intervals between the lower and upper bound of the data. The number
+    # of elements in the 1d array must be of one element larger than the
+    # shape of the data, because it includes the upper bound.
+
+    height, width = 20, 10
+    array_data = np.ones(shape=(height, width))
+    plot = Plot()
+
+    # bounds is None : infer from array_data shape
+    xs = plot._process_2d_bounds(None, array_data, 1)
+    assert xs.shape[0] == width + 1
+    ys = plot._process_2d_bounds(None, array_data, 0)
+    assert ys.shape[0] == height + 1
+
+    # bounds is a tuple : it defines lower and upper range
+    bounds = (1.0, 100.0)
+    xs = plot._process_2d_bounds(bounds, array_data, 1)
+    assert xs.shape[0] == width + 1
+    assert xs[0] == bounds[0] and xs[-1] == bounds[1]
+
+    # bounds is a 1D array: the first and last elements are used to create
+    # equally spaced intervals. Bounds must be of one element larger than the
+    # corresponding axis in array_data, or it will raise a Value error
+    bounds = np.zeros((height+1, ))
+    bounds[0], bounds[-1] = 0.2, 21.3
+    ys = plot._process_2d_bounds(bounds, array_data, 0)
+    assert ys.shape[0] == height + 1
+    assert ys[0] == bounds[0] and ys[-1] == bounds[-1]
+    with assert_raises(ValueError):
+        bounds = np.zeros((width // 2, ))
+        plot._process_2d_bounds(bounds, array_data, 0)
+
+    # bounds is a 2D array: the first and last elements along the appropriate
+    # axis are used to create equally spaced intervals.
+    # The size of the bounds must be the same as the data array, or this
+    # sill raise a ValueError
+    xbounds, ybounds = np.meshgrid(np.arange(width), np.arange(height))
+
+    xs = plot._process_2d_bounds(xbounds, array_data, 1)
+    assert xs.shape[0] == width + 1
+    assert xs[0] == xbounds[0,0] and xs[-2] == xbounds[0,-1]
+    with assert_raises(ValueError):
+        plot._process_2d_bounds(xbounds[:5,:], array_data, 1)
+
+    ys = plot._process_2d_bounds(ybounds, array_data, 0)
+    assert ys.shape[0] == height + 1
+    assert ys[0] == ybounds[0,0] and ys[-2] == ybounds[-1,0]
 
 
 if __name__ == '__main__':
-    import nose
     nose.main()
