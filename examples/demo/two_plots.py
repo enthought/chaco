e58b0032485593c8c9f63a8471ca3b7a0411464b
Warren Weckesser <warren.weckesser@enthought.com>
2012-09-24 11:24:22 -0500

    STY: PEP8 clean up.

--- examples/demo/two_plots.py
+++ examples/demo/two_plots.py
@@ -13,17 +13,19 @@ from traitsui.api import Item, Group, View
 from chaco.api import HPlotContainer, ArrayPlotData, Plot
 from chaco.tools.api import LineInspector, ZoomTool, PanTool
 
-#===============================================================================
+
+#==============================================================================
 # # Create the Chaco plot.
-#===============================================================================
+#==============================================================================
+
 def _create_plot_component():
 
     # Create the index
     numpoints = 100
     low = -5
     high = 15.0
-    x = arange(low, high, (high-low)/numpoints)
-    plotdata = ArrayPlotData(x=x, y1=jn(0,x), y2=jn(1,x))
+    x = arange(low, high, (high - low) / numpoints)
+    plotdata = ArrayPlotData(x=x, y1=jn(0, x), y2=jn(1, x))
 
     # Create the left plot
     left_plot = Plot(plotdata)
@@ -46,10 +48,13 @@ def _create_plot_component():
     right_plot.orientation = "v"
     right_plot.x_axis.title = "j1(x)"
     right_plot.y_axis.title = "X"
-    renderer2 = right_plot.plot(("x","y2"), type="line", color="red", width=2.0)[0]
+    renderer2 = right_plot.plot(("x", "y2"), type="line", color="red",
+                                width=2.0)[0]
     renderer2.index = renderer.index
-    renderer2.overlays.append(LineInspector(renderer2, write_metadata=True, is_listener=True))
-    renderer2.overlays.append(LineInspector(renderer2, axis="value", is_listener=True))
+    renderer2.overlays.append(LineInspector(renderer2, write_metadata=True,
+                              is_listener=True))
+    renderer2.overlays.append(LineInspector(renderer2, axis="value",
+                              is_listener=True))
     right_plot.overlays.append(ZoomTool(right_plot, tool_mode="range"))
     right_plot.tools.append(PanTool(right_plot))
 
@@ -59,31 +64,33 @@ def _create_plot_component():
 
     return container
 
-#===============================================================================
-# Attributes to use for the plot view.
-size=(750,500)
-title="Two Plots"
 
-#===============================================================================
+#==============================================================================
+# Attributes to use for the plot view.
+size = (750, 500)
+title = "Two Plots"
+
+
+#==============================================================================
 # # Demo class that is used by the demo.py application.
-#===============================================================================
+#==============================================================================
+
 class Demo(HasTraits):
+
     plot = Instance(Component)
 
     traits_view = View(
                     Group(
                         Item('plot', editor=ComponentEditor(size=size),
                              show_label=False),
-                        orientation = "vertical"),
+                        orientation="vertical"),
                     resizable=True, title=title,
                     )
 
     def _plot_default(self):
-         return _create_plot_component()
+        return _create_plot_component()
 
 demo = Demo()
 
 if __name__ == "__main__":
     demo.configure_traits()
-
-# EOF
