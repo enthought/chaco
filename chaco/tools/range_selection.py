d9aa1362531a0d4e21721700377e1387556ad3f5
tonysyu <tsyu80@gmail.com>
2013-02-22 12:29:38 -0600

    Fix resizing bug in range_selection tool
    Resizing didn't save a mouse-down point. This, combined with that fact that
    selections below the `minimum_selection` size get deselected resulted in
    strange errors. This also led to exceptions if the event state was set to
    "selected" before initial resize event.

--- chaco/tools/range_selection.py
+++ chaco/tools/range_selection.py
@@ -207,6 +207,10 @@ class RangeSelection(AbstractController):
         if self.enable_resize:
             if (abs(mouse_coord - high) <= self.resize_margin) or \
                             (abs(mouse_coord - low) <= self.resize_margin):
+                # HACK: Choose down point that is far enough away from the
+                # boundaries that resizing will never be deselected.
+                x_out = low - self.resize_margin - self.minimum_selection
+                self._down_point = array([x_out, x_out])
                 return self.selected_right_down(event)
 
         if tmp[self.axis_index] >= low and tmp[self.axis_index] <= high:
@@ -458,7 +462,6 @@ class RangeSelection(AbstractController):
         if self.minimum_selection > abs(start - end):
             self.deselect(event)
             event.handled = True
-
         else:
             self.event_state = "selected"
 
