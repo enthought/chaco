8620df8f11b55dd7bb8edbc48cf6c1b8632160b2
Adam Hughes <hugadams@gwmail.gwu.edu>
2013-02-27 13:57:38 -0500

    Added event handling for added and removed events in update handler of Plot class.  Also modified update handler for changed event to raise error if key not found, instead of just passing.

--- chaco/plot.py
+++ chaco/plot.py
@@ -1032,16 +1032,21 @@ class Plot(DataView):
     def _data_update_handler(self, name, event):
         # event should be a dict with keys "added", "removed", and "changed",
         # per the comments in AbstractPlotData.
-        if event.has_key("added"):
-            pass
-
         if event.has_key("removed"):
-            pass
+            for name in event["removed"]:
+		del self.datasources[name]
+
+        if event.has_key("added"):
+            for name in event["added"]:
+		self._get_or_create_datasource(name)
 
         if event.has_key("changed"):
             for name in event["changed"]:
-                if self.datasources.has_key(name):
+		try:
                     source = self.datasources[name]
+		except KeyError:
+		    raise KeyError('Datasource has no event "%s"'%name)
+		else:
                     source.set_data(self.data.get_data(name))
 
     def _plots_items_changed(self, event):
