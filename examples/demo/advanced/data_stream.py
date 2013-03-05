e1f998dd4feb0d16b58d14620a78fe3e57d0decd
tonysyu <tsyu80@gmail.com>
2013-03-05 17:30:49 -0600

    Remove wx-dependency from data_stream example.
    Reuse generic Traits classes instead of requiring a wx app. This change was
    thoroughly tested in ipython (with all permutations of wx/qt backends and
    ETS_TOOLKITs). This was run in Mac and Windows version of the IDE, and
    I checked demo.py with ETS_TOOLKIT set to wx and qt.

--- examples/demo/advanced/data_stream.py
+++ examples/demo/advanced/data_stream.py
@@ -1,5 +1,5 @@
 """
-Sample visualization of simulated live data stream
+Visualization of simulated live data stream
 
 Shows how Chaco and Traits can be used to easily build a data
 acquisition and visualization system.
@@ -12,13 +12,11 @@ by the user.
 """
 
 # Major library imports
-import random
-import wx
-from numpy import arange, array, hstack, random
+import numpy as np
 
 # Enthought imports
-from traits.api import Array, Bool, Callable, Enum, Float, HasTraits, \
-                                 Instance, Int, Trait
+from traits.api import (Array, Callable, Enum, Float, HasTraits, Instance, Int,
+                        Trait)
 from traitsui.api import Group, HGroup, Item, View, spring, Handler
 from pyface.timer.api import Timer
 
@@ -31,30 +29,26 @@ class Viewer(HasTraits):
     by the Controller.  The visualization/editor for this class is a
     Chaco plot.
     """
-
     index = Array
 
     data = Array
 
     plot_type = Enum("line", "scatter")
 
-    # This "view" attribute defines how an instance of this class will
-    # be displayed when .edit_traits() is called on it.  (See MyApp.OnInit()
-    # below.)
     view = View(ChacoPlotItem("index", "data",
-                               type_trait="plot_type",
-                               resizable=True,
-                               x_label="Time",
-                               y_label="Signal",
-                               color="blue",
-                               bgcolor="white",
-                               border_visible=True,
-                               border_width=1,
-                               padding_bg_color="lightgray",
-                               width=800,
-                               height=380,
-                               marker_size=2,
-                               show_label=False),
+                              type_trait="plot_type",
+                              resizable=True,
+                              x_label="Time",
+                              y_label="Signal",
+                              color="blue",
+                              bgcolor="white",
+                              border_visible=True,
+                              border_width=1,
+                              padding_bg_color="lightgray",
+                              width=800,
+                              height=380,
+                              marker_size=2,
+                              show_label=False),
                 HGroup(spring, Item("plot_type", style='custom'), spring),
                 resizable = True,
                 buttons = ["OK"],
@@ -82,7 +76,7 @@ class Controller(HasTraits):
     # just means that self._generator should be initialized to
     # random.normal, which is a random number function, and in the future
     # it can be set to any callable object.
-    _generator = Trait(random.normal, Callable)
+    _generator = Trait(np.random.normal, Callable)
 
     view = View(Group('distribution_type',
                       'mean',
@@ -92,9 +86,10 @@ class Controller(HasTraits):
                       buttons=["OK", "Cancel"])
 
     def timer_tick(self, *args):
-        """ Callback function that should get called based on a wx timer
-        tick.  This will generate a new random datapoint and set it on
-        the .data array of our viewer object.
+        """
+        Callback function that should get called based on a timer tick.  This
+        will generate a new random data point and set it on the `.data` array
+        of our viewer object.
         """
         # Generate a new number and increment the tick count
         new_val = self._generator(self.mean, self.stddev)
@@ -103,8 +98,9 @@ class Controller(HasTraits):
         # grab the existing data, truncate it, and append the new point.
         # This isn't the most efficient thing in the world but it works.
         cur_data = self.viewer.data
-        new_data = hstack((cur_data[-self.max_num_points+1:], [new_val]))
-        new_index = arange(self.num_ticks - len(new_data) + 1, self.num_ticks+0.01)
+        new_data = np.hstack((cur_data[-self.max_num_points+1:], [new_val]))
+        new_index = np.arange(self.num_ticks - len(new_data) + 1,
+                              self.num_ticks + 0.01)
 
         self.viewer.index = new_index
         self.viewer.data = new_data
@@ -113,17 +109,10 @@ class Controller(HasTraits):
     def _distribution_type_changed(self):
         # This listens for a change in the type of distribution to use.
         if self.distribution_type == "normal":
-            self._generator = random.normal
+            self._generator = np.random.normal
         else:
-            self._generator = random.lognormal
+            self._generator = np.random.lognormal
 
-#===============================================================================
-# # Demo class that is used by the demo.py application.
-#===============================================================================
-# NOTE: The Demo class is being created for the purpose of running this
-# example using a TraitsDemo-like app (see examples/demo/demo.py in Traits3).
-# The demo.py file looks for a 'demo' or 'popup' or 'modal popup' keyword
-# when it executes this file, and displays a view for it.
 
 class DemoHandler(Handler):
 
@@ -135,13 +124,14 @@ class DemoHandler(Handler):
         info.object.timer.Stop()
         return
 
+
 class Demo(HasTraits):
     controller = Instance(Controller)
     viewer = Instance(Viewer, ())
     timer = Instance(Timer)
     view = View(Item('controller', style='custom', show_label=False),
                 Item('viewer', style='custom', show_label=False),
-                handler = DemoHandler,
+                handler=DemoHandler,
                 resizable=True)
 
     def edit_traits(self, *args, **kws):
@@ -159,43 +149,11 @@ class Demo(HasTraits):
     def _controller_default(self):
         return Controller(viewer=self.viewer)
 
+
+# NOTE: examples/demo/demo.py looks for a 'demo' or 'popup' or 'modal popup'
+# keyword when it executes this file, and displays a view for it.
 popup=Demo()
 
-# wxApp used when this file is run from the command line.
 
-class MyApp(wx.PySimpleApp):
-
-    def OnInit(self, *args, **kw):
-        viewer = Viewer()
-        controller = Controller(viewer = viewer)
-
-        # Pop up the windows for the two objects
-        viewer.edit_traits()
-        controller.edit_traits()
-
-        # Set up the timer and start it up
-        self.setup_timer(controller)
-        return True
-
-
-    def setup_timer(self, controller):
-        # Create a new WX timer
-        timerId = wx.NewId()
-        self.timer = wx.Timer(self, timerId)
-
-        # Register a callback with the timer event
-        self.Bind(wx.EVT_TIMER, controller.timer_tick, id=timerId)
-
-        # Start up the timer!  We have to tell it how many milliseconds
-        # to wait between timer events.  For now we will hardcode it
-        # to be 100 ms, so we get 10 points per second.
-        self.timer.Start(100.0, wx.TIMER_CONTINUOUS)
-        return
-
-
-# This is called when this example is to be run in a standalone mode.
 if __name__ == "__main__":
-    app = MyApp()
-    app.MainLoop()
-
-# EOF
+    popup.configure_traits()
