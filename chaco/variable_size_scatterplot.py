9846b4460cde52c5f8d1128801c96b3637f7ddc6
Pietro Berkes <pberkes@enthought.com>
2013-03-10 12:01:20 +0000

    Correct typo, comment about deprecating class.

--- chaco/variable_size_scatterplot.py
+++ chaco/variable_size_scatterplot.py
@@ -1,9 +1,10 @@
 """ The base ScatterPlot class now accepts variable sized markers.
 
-This definition remains for backwards compatability.
+This definition remains for backwards compatibility.
 """
 from chaco.scatterplot import ScatterPlot
 
 
+# TODO: This should be officially deprecated.
 class VariableSizeScatterPlot(ScatterPlot):
     pass
