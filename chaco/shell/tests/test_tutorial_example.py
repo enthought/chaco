e331a948aa98bf385805f22ccc75a835250ffc72
Kelsey Jordahl <kjordahl@enthought.com>
2013-10-27 22:17:34 -0400

    TST: Add a test to exercise the interactive shell example from the tutorial

---
+++ chaco/shell/tests/test_tutorial_example.py
@@ -0,0 +1,21 @@
+""" Test script-oriented example from interactive plotting tutorial
+
+source: docs/source/user_manual/chaco_tutorial.rst
+
+"""
+import unittest
+from numpy import linspace, pi, sin
+from enthought.chaco.shell import plot, show, title, ytitle
+
+
+class InteractiveTestCase(unittest.TestCase):
+
+    def test_script(self):
+        x = linspace(-2*pi, 2*pi, 100)
+        y = sin(x)
+        plot(x, y, "r-")
+        title("First plot")
+        ytitle("sin(x)")
+
+if __name__ == "__main__":
+    unittest.main()
