407d99e30f38c0df54585cc7572d20305b874e8d
Kelsey Jordahl <kjordahl@enthought.com>
2013-11-04 07:49:41 -0500

    TST: Workaround for assertLess in python 2.6

--- chaco/scales/scales_test_case.py
+++ chaco/scales/scales_test_case.py
@@ -167,7 +167,11 @@ class BasicFormatterTestCase(TicksTestCase):
             labels = fmt.format(ticks, numlabels, None)
             actual = sum(map(len, labels))
             err = abs(estimate - actual) / actual
-            self.assertLess(err, 0.5)
+            try:
+                self.assertLess(err, 0.5)
+            except AttributeError:
+                # Python 2.6
+                self.assert_(err < 0.5)
         return
 
     def test_width_based_default_scale(self):
