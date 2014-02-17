4f59c2bb8eb8d193c5922518654def9016f22fd2
Corran Webster <cwebster@enthought.com>
2014-02-17 17:02:48 -0600

    PEP8 fixes to test.

--- chaco/tests/scatterplot_renderers_test_case.py
+++ chaco/tests/scatterplot_renderers_test_case.py
@@ -10,7 +10,7 @@ from chaco.api import create_scatter_plot, PlotGraphicsContext
 class DrawScatterplotCase(unittest.TestCase):
     def test_scatter_fast(self):
         """ Coverage test to check basic case works """
-        size = (50,50)
+        size = (50, 50)
         scatterplot = create_scatter_plot(
             data=[range(10), range(10)],
             border_visible=False,
@@ -18,13 +18,13 @@ class DrawScatterplotCase(unittest.TestCase):
         scatterplot.outer_bounds = list(size)
         gc = PlotGraphicsContext(size)
         gc.render_component(scatterplot)
-        actual = gc.bmp_array[:,:,:]
+        actual = gc.bmp_array[:, :, :]
         #gc.save('scatter.png')
         assert(not alltrue(actual == 255))
 
     def test_scatter_circle(self):
         """ Coverage test to check circles work """
-        size = (50,50)
+        size = (50, 50)
         scatterplot = create_scatter_plot(
             data=[range(10), range(10)],
             marker="circle",
@@ -33,7 +33,7 @@ class DrawScatterplotCase(unittest.TestCase):
         scatterplot.outer_bounds = list(size)
         gc = PlotGraphicsContext(size)
         gc.render_component(scatterplot)
-        actual = gc.bmp_array[:,:,:]
+        actual = gc.bmp_array[:, :, :]
         #gc.save('scatter.png')
         assert(not alltrue(actual == 255))
 
@@ -41,13 +41,13 @@ class DrawScatterplotCase(unittest.TestCase):
         """ Coverage test to check custom markers work """
         # build path
         path = CompiledPath()
-        path.move_to(-5,-5)
+        path.move_to(-5, -5)
         path.line_to(5, 5)
         path.line_to(5, -5)
         path.line_to(-5, 5)
         path.line_to(-5, -5)
 
-        size = (50,50)
+        size = (50, 50)
         scatterplot = create_scatter_plot(
             data=[range(10), range(10)],
             marker='custom',
@@ -57,22 +57,22 @@ class DrawScatterplotCase(unittest.TestCase):
         scatterplot.outer_bounds = list(size)
         gc = PlotGraphicsContext(size)
         gc.render_component(scatterplot)
-        actual = gc.bmp_array[:,:,:]
+        actual = gc.bmp_array[:, :, :]
         #gc.save('scatter.png')
         assert(not alltrue(actual == 255))
 
     def test_scatter_slow(self):
         """ Coverage test to check multiple marker size works """
-        size = (50,50)
+        size = (50, 50)
         scatterplot = create_scatter_plot(
             data=[range(10), range(10)],
             border_visible=False,
-            marker_size=range(1,11),
+            marker_size=range(1, 11),
         )
         scatterplot.outer_bounds = list(size)
         gc = PlotGraphicsContext(size)
         gc.render_component(scatterplot)
-        actual = gc.bmp_array[:,:,:]
+        actual = gc.bmp_array[:, :, :]
         #gc.save('scatter.png')
         assert(not alltrue(actual == 255))
 
