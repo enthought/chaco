724adc88815d1d58ecf82109eeb60cf5e4457cca
John Tyree <johntyree@gmail.com>
2014-05-29 18:50:00 -0500

    DOC: Add an html title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ html_style = 'default.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Chaco 4 User Manual"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
