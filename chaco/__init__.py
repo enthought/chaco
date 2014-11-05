8f98f4b308a6e8d31e973e879c085b92d16e38a5
Jonathan Rocher <jrocher@enthought.com>
2014-11-05 12:51:42 -0600

    Add a - to follow pattern in documentation.

--- chaco/__init__.py
+++ chaco/__init__.py
@@ -3,7 +3,7 @@
 """ Two-dimensional plotting application toolkit.
     Part of the Chaco project of the Enthought Tool Suite.
 """
-__version__ = '4.6.0dev'
+__version__ = '4.6.0-dev'
 
 __requires__ = [
    'enable',
