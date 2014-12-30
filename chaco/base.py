97a50e4c1c2c8550629c3feeaf9586718f4e7895
Corran Webster <cwebster@enthought.com>
2014-12-30 11:21:21 +0000

    Replace bin_search with a searchsorted-based algorithm. 5x speedup.

--- chaco/base.py
+++ chaco/base.py
@@ -60,34 +60,25 @@ def n_gon(center, r, nsides, rot_degrees=0):
     return [poly_point(center, r, i*theta+rotation) for i in range(nsides)]
 
 
-# Ripped from Chaco 1.0's plot_base.py
 def bin_search(values, value, ascending):
     """
     Performs a binary search of a sorted array looking for a specified value.
 
     Returns the lowest position where the value can be found or where the
     array value is the last value less (greater) than the desired value.
-    Returns -1 if *value* is beyond the minimum or maximum of *values*.
+    Returns -1 if `value` is beyond the minimum or maximum of `values`.
     """
     if ascending > 0:
         if (value < values[0]) or (value > values[-1]):
             return -1
+        index = values.searchsorted(value, 'right') - 1
     else:
         if (value < values[-1]) or (value > values[0]):
             return -1
-    lo = 0
-    hi = len( values )
-    while True:
-        mid  = (hi + lo) / 2
-        test = cmp( values[ mid ], value ) * ascending
-        if test == 0:
-            return mid
-        if test > 0:
-            hi = mid
-        else:
-            lo = mid
-        if lo >= (hi - 1):
-            return lo
+        ascending_values = values[::-1]
+        index = len(values) - ascending_values.searchsorted(value, 'left') - 1
+    return index
+
 
 def reverse_map_1d(data, pt, sort_order, floor_only=False):
     """Returns the index of *pt* in the array *data*.
