1727f3e64e55a36c593590bdfb4578771f66d369
Corran Webster <cwebster@enthought.com>
2015-01-07 13:19:26 +0000

    Add orientation tests for 1d plots.

--- chaco/tests/text_plot_1d_test_case.py
+++ chaco/tests/text_plot_1d_test_case.py
@@ -1,6 +1,7 @@
 import unittest
 
-from numpy import alltrue, arange
+from numpy import alltrue, arange, array
+from numpy.testing import assert_almost_equal
 from enable.compiled_path import CompiledPath
 
 # Chaco imports
@@ -28,6 +29,47 @@ class TextPlot1DTest(unittest.TestCase):
         self.scatterplot.outer_bounds = list(self.size)
 
     def test_scatter_1d(self):
+        self.assertEqual(self.scatterplot.origin, 'bottom left')
+        self.assertIsNone(self.scatterplot.x_mapper)
+        self.assertEqual(self.scatterplot.y_mapper, self.scatterplot.index_mapper)
+
+        gc = PlotGraphicsContext(self.size)
+        gc.render_component(self.scatterplot)
+        actual = gc.bmp_array[:, :, :]
+        self.assertFalse(alltrue(actual == 255))
+
+    def test_scatter_1d_horizontal(self):
+        self.scatterplot.orientation = 'h'
+
+        self.assertEqual(self.scatterplot.origin, 'bottom left')
+        self.assertEqual(self.scatterplot.x_mapper, self.scatterplot.index_mapper)
+        self.assertIsNone(self.scatterplot.y_mapper)
+
+        gc = PlotGraphicsContext(self.size)
+        gc.render_component(self.scatterplot)
+        actual = gc.bmp_array[:, :, :]
+        self.assertFalse(alltrue(actual == 255))
+
+    def test_scatter_1d_flipped(self):
+        self.scatterplot.direction = 'flipped'
+
+        self.assertEqual(self.scatterplot.origin, 'top left')
+        self.assertIsNone(self.scatterplot.x_mapper)
+        self.assertEqual(self.scatterplot.y_mapper, self.scatterplot.index_mapper)
+
+        gc = PlotGraphicsContext(self.size)
+        gc.render_component(self.scatterplot)
+        actual = gc.bmp_array[:, :, :]
+        self.assertFalse(alltrue(actual == 255))
+
+    def test_scatter_1d_horizontal_flipped(self):
+        self.scatterplot.direction = 'flipped'
+        self.scatterplot.orientation = 'h'
+
+        self.assertEqual(self.scatterplot.origin, 'bottom right')
+        self.assertEqual(self.scatterplot.x_mapper, self.scatterplot.index_mapper)
+        self.assertIsNone(self.scatterplot.y_mapper)
+
         gc = PlotGraphicsContext(self.size)
         gc.render_component(self.scatterplot)
         actual = gc.bmp_array[:, :, :]
@@ -39,3 +81,27 @@ class TextPlot1DTest(unittest.TestCase):
         gc.render_component(self.scatterplot)
         actual = gc.bmp_array[:, :, :]
         self.assertFalse(alltrue(actual == 255))
+
+    def test_scatter_1d_map_data(self):
+        points = array([[0, 124.5], [124.5, 0]])
+        assert_almost_equal(self.scatterplot.map_data(points),
+                            array([4.5, 0]))
+
+    def test_scatter_1d_map_data_horizontal(self):
+        self.scatterplot.orientation = 'h'
+        points = array([[0, 124.5], [124.5, 0]])
+        assert_almost_equal(self.scatterplot.map_data(points),
+                            array([0, 4.5]))
+
+    def test_scatter_1d_map_data_flipped(self):
+        self.scatterplot.direction = 'flipped'
+        points = array([[0, 124.5], [124.5, 0]])
+        assert_almost_equal(self.scatterplot.map_data(points),
+                            array([4.5, 9.0]))
+
+    def test_scatter_1d_map_data_horizontal_flipped(self):
+        self.scatterplot.direction = 'flipped'
+        self.scatterplot.orientation = 'h'
+        points = array([[0, 124.5], [124.5, 0]])
+        assert_almost_equal(self.scatterplot.map_data(points),
+                            array([9.0, 4.5]))
