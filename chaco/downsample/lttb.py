0d913496e2d5900745ba22786d95f352dc8b523c
Corran Webster <cwebster@enthought.com>
2016-10-18 10:55:31 +0100

    Implement largest triangle three buckets downsampling.

---
+++ chaco/downsample/lttb.py
@@ -0,0 +1,45 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+try:
+    from chaco.downsample import _lttb
+except ImportError:
+    _lttb = None
+    logger.warning(
+        "Can't import _lttb extension module, lttb downsampling will not work."
+    )
+
+
+def largest_triangle_three_buckets(points, n_buckets):
+    """ Apply the largest triangle three buckets algorithm to data points
+
+    Parameters
+    ----------
+    points : N, 2 array of float
+        The points as a N by 2 array of floats.  The index values (column 0)
+        must be monotone.
+    n_buckets : int
+        The number of buckets.
+
+    Returns
+    -------
+    index, values : array, array
+        The downsampled index and values.
+
+    References
+    ----------
+
+    Sveinn Steinarsson, "Down Sampling Time Series for Visual Representation,"
+    Master's Thesis, University of Iceland, 2013.
+    http://skemman.is/handle/1946/15343
+    """
+    if n_buckets > points.shape[0] or n_buckets <= 2:
+        return points
+
+    if _lttb is not None:
+        return _lttb.lttb(points, n_buckets)
+    else:
+        # can't downsample, do nothing, but better than crashing
+        return points
