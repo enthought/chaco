772e21998f724c4e3d8a173f1860f7f005a56d8c
Corran Webster <cwebster@enthought.com>
2016-11-16 16:21:44 +0000

    Add test case for vertex data and fix test for cell data.

--- chaco/tests_with_backend/create_2d_test_case.py
+++ chaco/tests_with_backend/create_2d_test_case.py
@@ -38,26 +38,26 @@ def test_bounds_2d_case():
         pv.edit_traits()
 
 
-def test_process_2d_bounds():
+def test_process_2d_bounds_cell_plot():
     # behavior: _process_2d_bounds accepts all possible ways to set x and y
     # bounds in 2d plots and returns a 1d array with equally spaced
     # intervals between the lower and upper bound of the data. The number
     # of elements in the 1d array must be of one element larger than the
-    # shape of the data, because it includes the upper bound.
+    # shape of the data, because this is cell data.
 
     height, width = 20, 10
     array_data = np.ones(shape=(height, width))
     plot = Plot()
 
     # bounds is None : infer from array_data shape
-    xs = plot._process_2d_bounds(None, array_data, 1)
+    xs = plot._process_2d_bounds(None, array_data, 1, cell_plot=True)
     assert xs.shape[0] == width + 1
-    ys = plot._process_2d_bounds(None, array_data, 0)
+    ys = plot._process_2d_bounds(None, array_data, 0, cell_plot=True)
     assert ys.shape[0] == height + 1
 
     # bounds is a tuple : it defines lower and upper range
     bounds = (1.0, 100.0)
-    xs = plot._process_2d_bounds(bounds, array_data, 1)
+    xs = plot._process_2d_bounds(bounds, array_data, 1, cell_plot=True)
     assert xs.shape[0] == width + 1
     assert xs[0] == bounds[0] and xs[-1] == bounds[1]
 
@@ -66,12 +66,66 @@ def test_process_2d_bounds():
     # corresponding axis in array_data, or it will raise a Value error
     bounds = np.zeros((height+1, ))
     bounds[0], bounds[-1] = 0.2, 21.3
-    ys = plot._process_2d_bounds(bounds, array_data, 0)
+    ys = plot._process_2d_bounds(bounds, array_data, 0, cell_plot=True)
     assert ys.shape[0] == height + 1
     assert ys[0] == bounds[0] and ys[-1] == bounds[-1]
     with assert_raises(ValueError):
         bounds = np.zeros((width // 2, ))
-        plot._process_2d_bounds(bounds, array_data, 0)
+        plot._process_2d_bounds(bounds, array_data, 0, cell_plot=True)
+
+    # bounds is a 2D array: the first and last elements along the appropriate
+    # axis are used to create equally spaced intervals.
+    # The size of the bounds must be the same as the data array, or this
+    # sill raise a ValueError
+    xbounds, ybounds = np.meshgrid(np.arange(width+1), np.arange(height+1))
+
+    xs = plot._process_2d_bounds(xbounds, array_data, 1, cell_plot=True)
+    assert xs.shape[0] == width + 1
+    assert xs[0] == xbounds[0,0] and xs[-1] == xbounds[0,-1]
+    with assert_raises(ValueError):
+        plot._process_2d_bounds(xbounds[:, :5], array_data, 1, cell_plot=True)
+
+    ys = plot._process_2d_bounds(ybounds, array_data, 0, cell_plot=True)
+    assert ys.shape[0] == height + 1
+    assert ys[0] == ybounds[0,0] and ys[-1] == ybounds[-1,0]
+    with assert_raises(ValueError):
+        plot._process_2d_bounds(ybounds[:5, :], array_data, 0, cell_plot=True)
+
+
+def test_process_2d_bounds_vertex_data():
+    # behavior: _process_2d_bounds accepts all possible ways to set x and y
+    # bounds in 2d plots and returns a 1d array with equally spaced
+    # intervals between the lower and upper bound of the data. The number
+    # of elements in the 1d array must be the same as the shape of the data,
+    # because this is vertex data.
+
+    height, width = 20, 10
+    array_data = np.ones(shape=(height, width))
+    plot = Plot()
+
+    # bounds is None : infer from array_data shape
+    xs = plot._process_2d_bounds(None, array_data, 1, cell_plot=False)
+    assert xs.shape[0] == width
+    ys = plot._process_2d_bounds(None, array_data, 0, cell_plot=False)
+    assert ys.shape[0] == height
+
+    # bounds is a tuple : it defines lower and upper range
+    bounds = (1.0, 100.0)
+    xs = plot._process_2d_bounds(bounds, array_data, 1, cell_plot=False)
+    assert xs.shape[0] == width
+    assert xs[0] == bounds[0] and xs[-1] == bounds[1]
+
+    # bounds is a 1D array: the first and last elements are used to create
+    # equally spaced intervals. Bounds must be of one element larger than the
+    # corresponding axis in array_data, or it will raise a Value error
+    bounds = np.zeros((height, ))
+    bounds[0], bounds[-1] = 0.2, 21.3
+    ys = plot._process_2d_bounds(bounds, array_data, 0, cell_plot=False)
+    assert ys.shape[0] == height
+    assert ys[0] == bounds[0] and ys[-1] == bounds[-1]
+    with assert_raises(ValueError):
+        bounds = np.zeros((width // 2, ))
+        plot._process_2d_bounds(bounds, array_data, 0, cell_plot=False)
 
     # bounds is a 2D array: the first and last elements along the appropriate
     # axis are used to create equally spaced intervals.
@@ -79,15 +133,17 @@ def test_process_2d_bounds():
     # sill raise a ValueError
     xbounds, ybounds = np.meshgrid(np.arange(width), np.arange(height))
 
-    xs = plot._process_2d_bounds(xbounds, array_data, 1)
-    assert xs.shape[0] == width + 1
-    assert xs[0] == xbounds[0,0] and xs[-2] == xbounds[0,-1]
+    xs = plot._process_2d_bounds(xbounds, array_data, 1, cell_plot=False)
+    assert xs.shape[0] == width
+    assert xs[0] == xbounds[0,0] and xs[-1] == xbounds[0,-1]
     with assert_raises(ValueError):
-        plot._process_2d_bounds(xbounds[:5,:], array_data, 1)
+        plot._process_2d_bounds(xbounds[:, :5], array_data, 1, cell_plot=False)
 
-    ys = plot._process_2d_bounds(ybounds, array_data, 0)
-    assert ys.shape[0] == height + 1
-    assert ys[0] == ybounds[0,0] and ys[-2] == ybounds[-1,0]
+    ys = plot._process_2d_bounds(ybounds, array_data, 0, cell_plot=False)
+    assert ys.shape[0] == height
+    assert ys[0] == ybounds[0,0] and ys[-1] == ybounds[-1,0]
+    with assert_raises(ValueError):
+        plot._process_2d_bounds(ybounds[:5, :], array_data, 0, cell_plot=False)
 
 
 if __name__ == '__main__':
