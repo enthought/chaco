15ffec10eb2fb374f7f415f3bff34f3d195dbe15
spichardo <spichard@lakeheadu.ca>
2016-12-14 16:07:40 -0500

    A couple of fixes to keep VS2010 compatibility and avoid a deprecated warnings with newer versions of numpy
    The first fix is on chaco\contour\cntrc.c to move the moduledef
    declaration to the top the MOD_INIT function to be complain with older
    compilers followingn the old C89 specification (such as VS 2010), the
    credit to spot this goes to @stevenengler . The change should be
    compatible with any other compiler.
    
    The second change is on chaco\base.py to avoid a deprecate warning on
    newer versions of numpy that do not accept differantion of boolean
    arrays. The current fix simply recasted the array as uint8.

--- chaco/base.py
+++ chaco/base.py
@@ -7,7 +7,7 @@ from math import radians, sqrt
 
 # Major library imports
 from numpy import (array, argsort, concatenate, cos, diff, dot, empty, isfinite,
-                   nonzero, pi, searchsorted, seterr, sin)
+                   nonzero, pi, searchsorted, seterr, sin, uint8)
 
 # Enthought library imports
 from traits.api import CArray, Enum, Trait
@@ -220,7 +220,7 @@ def arg_true_runs(bool_array):
     """ Find runs where array is True """
     if len(bool_array) == 0:
         return []
-    runs = arg_find_runs(bool_array, 'flat')
+    runs = arg_find_runs(bool_array.astype(uint8), 'flat')
     # runs have to alternate true and false
     if bool_array[0]:
         # even runs are true
