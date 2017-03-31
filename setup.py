8058ac00844f63cc6567a4c826cfbdbc423c7dd1
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2017-03-31 13:01:06 +0100

    MAINT: bump version number/switch back to unreleased.

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ from setuptools import setup, Extension, find_packages
 
 MAJOR = 4
 MINOR = 6
-MICRO = 1
+MICRO = 2
 
-IS_RELEASED = True
+IS_RELEASED = False
 
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
