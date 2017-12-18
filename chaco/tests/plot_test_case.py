8f59ecc6278261722612dcdc06fa1346cb5b0b61
Robert Kern <rkern@enthought.com>
2017-12-18 12:34:35 -0800

    BUG: Fix use of itertools.chain()
    Introduced in #369
    Fixes #393

--- chaco/tests/plot_test_case.py
+++ chaco/tests/plot_test_case.py
@@ -3,7 +3,7 @@ import unittest
 from numpy import arange
 
 # Chaco imports
-from chaco.api import ArrayPlotData, Plot
+from chaco.api import ArrayPlotData, Plot, DataRange1D
 
 
 class PlotTestCase(unittest.TestCase):
@@ -17,6 +17,19 @@ class PlotTestCase(unittest.TestCase):
         arr = arange(16).reshape(2, 2, 2, 2)
         data.update_data(x=arr, y=arr)
         self.assertRaises(ValueError, plot.plot, ("x", "y"))
+
+    def test_range_change(self):
+        arr = arange(10)
+        data = ArrayPlotData(x=arr, y=arr)
+        plot = Plot(data)
+        renderer = plot.plot(('x', 'y'))[0]
+        new_range = DataRange1D()
+        old_range = plot.index_range
+        self.assertIsNot(old_range, new_range)
+        self.assertIs(renderer.index_range, old_range)
+        plot.index_range = new_range
+        self.assertIs(plot.index_range, new_range)
+        self.assertIs(renderer.index_range, new_range)
 
 if __name__ == "__main__":
     unittest.main()
