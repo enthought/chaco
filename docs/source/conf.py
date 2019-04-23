834d615c4f3afe2b96eebc5e167c15dc517be1bb
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2019-04-23 23:57:23 +0100

    MAINT: Remove custom Sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,11 +24,6 @@ def get_build_docset():
 
 BUILD_DOCSET = get_build_docset()
 
-# If your extensions are in another directory, add it here. If the directory
-# is relative to the documentation root, use os.path.abspath to make it
-# absolute, like shown here.
-sys.path.append(os.path.abspath('sphinxext'))
-
 # General configuration
 # ---------------------
 
