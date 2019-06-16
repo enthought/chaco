8c28301db095abc1758d5e7adfa554df74acca38
Jonathan Rocher <jrocher@kbibiopharma.com>
2019-06-16 16:22:34 -0500

    Expose the 2 overlays in the api module.

--- chaco/overlays/api.py
+++ chaco/overlays/api.py
@@ -2,5 +2,7 @@ from .databox import DataBox
 from .container_overlay import ContainerOverlay
 from .aligned_container_overlay import AlignedContainerOverlay
 from .text_grid_overlay import TextGridOverlay
-from .simple_inspector_overlay import SimpleInspectorOverlay, basic_formatter, \
+from ..text_box_overlay import TextBoxOverlay
+from ..tools.image_inspector_tool import ImageInspectorOverlay
+from .simple_inspector_overlay import SimpleInspectorOverlay, basic_formatter,\
     datetime_formatter, date_formatter, time_formatter
