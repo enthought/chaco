dcb6cb86a82a2c43c1d9f5b2cef5037aefd9988a
Nicola De Mitri <ndemitri@enthought.com>
2019-11-18 12:03:49 +0000

    ENH: +depends_on

--- chaco/base_1d_plot.py
+++ chaco/base_1d_plot.py
@@ -33,7 +33,7 @@ class Base1DPlot(AbstractPlotRenderer):
     index_mapper = Instance(AbstractMapper)
 
     #: Convenience property for accessing the data range of the mapper.
-    index_range = Property
+    index_range = Property(depends_on="index_mapper.range")
 
     #: Corresponds to either **index_mapper** or None, depending on
     #: the orientation of the plot.
