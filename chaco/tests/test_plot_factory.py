c6e300a81d81b63056fa538492248854de604f00
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-03-03 11:18:37 -0600

    Add tests.

---
+++ chaco/tests/test_plot_factory.py
@@ -0,0 +1,53 @@
+
+from unittest import TestCase
+import numpy as np
+
+from chaco.plot_factory import create_bar_plot, create_line_plot, \
+    create_polar_plot, create_scatter_plot
+from chaco.api import BarPlot, LinePlot, PlotAxis, PlotGrid, ScatterPlot
+
+x = np.array([1, 2, 3, 4])
+
+y = np.array([1, 2, 3, 4])
+
+
+class BaseTestRenderer(object):
+    def test_create_renderer_default(self):
+        renderer = self.factory((x, y))
+        self.assertIsInstance(renderer, self.renderer_klass)
+
+    def test_create_renderer_additional_traits(self):
+        renderer = self.factory((x, y), alpha=0.5)
+        self.assertIsInstance(renderer, self.renderer_klass)
+
+    def test_create_renderer_add_axis(self):
+        renderer = self.factory((x, y), add_axis=True)
+        self.assertIsInstance(renderer, self.renderer_klass)
+        self.assertEqual(len(renderer.underlays), 2)
+        for underlay in renderer.underlays:
+            self.assertIsInstance(underlay, PlotAxis)
+
+    def test_create_renderer_add_grids(self):
+        renderer = self.factory((x, y), add_grids=True)
+        self.assertIsInstance(renderer, self.renderer_klass)
+        self.assertEqual(len(renderer.underlays), 2)
+        for underlay in renderer.underlays:
+            self.assertIsInstance(underlay, PlotGrid)
+
+
+class TestCreateLineRenderer(BaseTestRenderer, TestCase):
+    def setUp(self):
+        self.factory = create_line_plot
+        self.renderer_klass = LinePlot
+
+
+class TestCreateScatterRenderer(BaseTestRenderer, TestCase):
+    def setUp(self):
+        self.factory = create_scatter_plot
+        self.renderer_klass = ScatterPlot
+
+
+class TestCreateBarRenderer(BaseTestRenderer, TestCase):
+    def setUp(self):
+        self.factory = create_bar_plot
+        self.renderer_klass = BarPlot
