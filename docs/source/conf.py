4d5fdcc9e62f37fa2e53fa8a2477965b9cd8aae9
aaronayres35 <36972686+aaronayres35@users.noreply.github.com>
2021-03-10 06:23:11 -0700

    Update docs conf.py - use Enthought sphinx template if available. (#571)
    * Update configuration; use Enthought sphinx template if available.
    
    * add newline at end of file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,13 @@ BUILD_DOCSET = get_build_docset()
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'traits.util.trait_documenter']
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.napoleon',
+  'sphinx.ext.graphviz',
+  'sphinx.ext.intersphinx',
+  'traits.util.trait_documenter',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -87,32 +93,26 @@ pygments_style = 'sphinx'
 # Options for HTML output
 # -----------------------
 
-# When using docset browsers like Dash and Zeal the side bar is redundant.
-if BUILD_DOCSET:
-    html_theme_options = {
-        'nosidebar': 'true'
-    }
-
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'default.css'
+#html_style = 'default.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "Chaco {}".format(version)
+#html_title = "Chaco {}".format(version)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
 # The name of an image file (within the static path) to place at the top of
 # the sidebar.
-html_logo = '_static/e-logo-rev.png'
+#html_logo = '_static/e-logo-rev.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'et.ico'
+#html_favicon = 'et.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -157,7 +157,34 @@ html_use_index = BUILD_DOCSET
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Chacodoc'
 
-html_theme='classic'
+try:
+    import enthought_sphinx_theme
+
+    html_theme_path = [enthought_sphinx_theme.theme_path]
+    html_theme = 'enthought'
+    # When using docset browsers like Dash and Zeal the side bar is redundant.
+    if BUILD_DOCSET:
+        html_theme_options = {
+            'sidebar': 'none'
+        }
+except ImportError as exc:
+    import warnings
+    msg = '''Can't find Enthought Sphinx Theme, using default.
+            Exception was: {}
+            Enthought Sphinx Theme can be downloaded from
+            https://github.com/enthought/enthought-sphinx-theme'''
+    warnings.warn(RuntimeWarning(msg.format(exc)))
+
+    # old defaults
+    html_logo = "e-logo-rev.jpg"
+    html_favicon = "et.png"
+    html_style = 'default.css'
+    html_theme = 'classic'
+    if BUILD_DOCSET:
+        html_theme_options = {
+            'nosidebar': 'true'
+        }
+
 
 # Options for LaTeX output
 # ------------------------
@@ -191,3 +218,8 @@ latex_documents = [
 
 # If false, no module index is generated.
 #latex_use_modindex = True
+
+
+intersphinx_mapping = {
+    'enable': ('http://docs.enthought.com/enable', None)
+}
