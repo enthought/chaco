7b54518e3a14ef49bbfe1a8c234b124b76ac8c1f
Aaron Ayres <aayres@enthought.com>
2021-04-15 13:22:14 -0700

    Revert "be more explicit about traits being optional in observer expression"
    This reverts commit 306e8d99473db314a5abffee7bb76876d586cf78.

--- chaco/tools/range_selection.py
+++ chaco/tools/range_selection.py
@@ -20,7 +20,6 @@ from traits.api import (
     Str,
     Trait,
 )
-from traits.observation.api import trait
 from enable.api import KeySpec
 
 # Chaco imports
@@ -679,11 +678,10 @@ class RangeSelection(AbstractController):
                 return 0
 
     @observe([
-        trait("component").then(trait("index_mapper") | trait("value_mapper")),
-        trait("_plot").then(
-            trait("index_mapper", optional=True) \
-            | trait("value_mapper", optional=True)
-        ),
+        "component.index_mapper",
+        "component.value_mapper",
+        "_plot.index_mapper",
+        "_plot.value_mapper"
     ])
     def _axis_mapper_updated(self, event):
         if event.name == self.axis + "_mapper":
