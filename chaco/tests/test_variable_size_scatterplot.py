14b731b7c6113fbada0651b1e0dd9832feded31e
Aaron Ayres <aayres@enthought.com>
2021-04-19 11:47:50 -0700

    flake8

--- chaco/tests/test_variable_size_scatterplot.py
+++ chaco/tests/test_variable_size_scatterplot.py
@@ -1,8 +1,9 @@
 """ Test for the deprecation of VariableSizeScatterPlot """
 import unittest
 
+
 class TestDeprecated(unittest.TestCase):
 
     def test_deprecation_warning(self):
         with self.assertWarns(DeprecationWarning):
-            from chaco.api import VariableSizeScatterPlot
+            from chaco.api import VariableSizeScatterPlot  # noqa: F401
