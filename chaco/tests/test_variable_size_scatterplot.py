4f6d380fa8c4d0b8aaa68f44bd2603436d8237aa
Aaron Ayres <aayres@enthought.com>
2021-04-19 12:19:50 -0700

    move deprecation warning to inside __init__ method

--- chaco/tests/test_variable_size_scatterplot.py
+++ chaco/tests/test_variable_size_scatterplot.py
@@ -1,9 +1,11 @@
 """ Test for the deprecation of VariableSizeScatterPlot """
 import unittest
 
+from chaco.api import VariableSizeScatterPlot
+
 
 class TestDeprecated(unittest.TestCase):
 
     def test_deprecation_warning(self):
         with self.assertWarns(DeprecationWarning):
-            from chaco.api import VariableSizeScatterPlot  # noqa: F401
+            VariableSizeScatterPlot()
--- chaco/variable_size_scatterplot.py
+++ chaco/variable_size_scatterplot.py
@@ -4,15 +4,15 @@ This definition remains for backwards compatibility.
 """
 import warnings
 
-warnings.warn(
-    "VariableSizeScatterPlot has been deprecated. Use the base ScatterPlot "
-    "class which now accepts variable sized markers",
-    DeprecationWarning
-)
-
 from .scatterplot import ScatterPlot
 
 
-# TODO: This should be officially deprecated.
 class VariableSizeScatterPlot(ScatterPlot):
-    pass
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "VariableSizeScatterPlot has been deprecated. Use the base "
+            "ScatterPlot class which now accepts variable sized markers",
+            DeprecationWarning
+        )
+        super(VariableSizeScatterPlot, self).__init__(*args, **kwargs)
