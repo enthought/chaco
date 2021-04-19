a106397dda050e140b89dd9463ec45a0c77b60b3
Aaron Ayres <aayres@enthought.com>
2021-04-19 12:12:29 -0700

    flake8

--- chaco/tests/test_plot_frames.py
+++ chaco/tests/test_plot_frames.py
@@ -6,19 +6,19 @@ import warnings
 class TestDeprecated(unittest.TestCase):
 
     def test_deprecation_warnings(self):
-        with warnings.catch_warnings(record=True) as warns:
+        with warnings.catch_warnings():
             warnings.simplefilter("error")
             with self.assertRaises(DeprecationWarning):
                 from chaco.api import BasePlotFrame  # noqa: F401
 
     def test_cross_deprecation_warning(self):
-        with warnings.catch_warnings(record=True) as warns:
+        with warnings.catch_warnings():
             warnings.simplefilter("error")
             with self.assertRaises(DeprecationWarning):
                 from chaco.api import CrossPlotFrame  # noqa: F401
 
     def test_simple_deprecation_warning(self):
-        with warnings.catch_warnings(record=True) as warns:
+        with warnings.catch_warnings():
             warnings.simplefilter("error")
             with self.assertRaises(DeprecationWarning):
                 from chaco.api import SimplePlotFrame  # noqa: F401
