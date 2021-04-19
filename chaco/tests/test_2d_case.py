3b16d47a2e3140d3b3206cb2c5e7262d38bce1a3
Poruri Sai Rahul <rporuri@enthought.com>
2021-04-19 15:56:20 +0530

    CLN : Fix flake8 errors in test_2d_case module
    	modified:   chaco/tests/test_2d_case.py
    	modified:   tox.ini

--- chaco/tests/test_2d_case.py
+++ chaco/tests/test_2d_case.py
@@ -64,8 +64,8 @@ class Test2DCase(unittest.TestCase):
         self.assertEqual(xs[-1], bounds[1])
 
         # bounds is a 1D array: the first and last elements are used to create
-        # equally spaced intervals. Bounds must be of one element larger than the
-        # corresponding axis in array_data, or it will raise a Value error
+        # equally spaced intervals. Bounds must be of one element larger than
+        # the corresponding axis in array_data, or it will raise a Value error
         bounds = np.zeros((height + 1,))
         bounds[0], bounds[-1] = 0.2, 21.3
         ys = plot._process_2d_bounds(bounds, array_data, 0, cell_plot=True)
@@ -76,8 +76,8 @@ class Test2DCase(unittest.TestCase):
             bounds = np.zeros((width // 2,))
             plot._process_2d_bounds(bounds, array_data, 0, cell_plot=True)
 
-        # bounds is a 2D array: the first and last elements along the appropriate
-        # axis are used to create equally spaced intervals.
+        # bounds is a 2D array: the first and last elements along the
+        # appropriate axis are used to create equally spaced intervals.
         # The size of the bounds must be the same as the data array, or this
         # sill raise a ValueError
         xbounds, ybounds = np.meshgrid(
@@ -106,8 +106,8 @@ class Test2DCase(unittest.TestCase):
         # behavior: _process_2d_bounds accepts all possible ways to set x and y
         # bounds in 2d plots and returns a 1d array with equally spaced
         # intervals between the lower and upper bound of the data. The number
-        # of elements in the 1d array must be the same as the shape of the data,
-        # because this is vertex data.
+        # of elements in the 1d array must be the same as the shape of the
+        # data, because this is vertex data.
 
         height, width = 20, 10
         array_data = np.ones(shape=(height, width))
@@ -127,8 +127,8 @@ class Test2DCase(unittest.TestCase):
         self.assertEqual(xs[-1], bounds[1])
 
         # bounds is a 1D array: the first and last elements are used to create
-        # equally spaced intervals. Bounds must be of one element larger than the
-        # corresponding axis in array_data, or it will raise a Value error
+        # equally spaced intervals. Bounds must be of one element larger than
+        # the corresponding axis in array_data, or it will raise a Value error
         bounds = np.zeros((height,))
         bounds[0], bounds[-1] = 0.2, 21.3
         ys = plot._process_2d_bounds(bounds, array_data, 0, cell_plot=False)
@@ -139,8 +139,8 @@ class Test2DCase(unittest.TestCase):
             bounds = np.zeros((width // 2,))
             plot._process_2d_bounds(bounds, array_data, 0, cell_plot=False)
 
-        # bounds is a 2D array: the first and last elements along the appropriate
-        # axis are used to create equally spaced intervals.
+        # bounds is a 2D array: the first and last elements along the
+        # appropriate axis are used to create equally spaced intervals.
         # The size of the bounds must be the same as the data array, or this
         # sill raise a ValueError
         xbounds, ybounds = np.meshgrid(np.arange(width), np.arange(height))
