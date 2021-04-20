5f2a8c8cd26664600f384e433f7ca7037a52966a
Aaron Ayres <aayres@enthought.com>
2021-04-20 13:18:46 -0700

    update domain_limits.py

--- examples/demo/domain_limits.py
+++ examples/demo/domain_limits.py
@@ -9,7 +9,7 @@ from chaco.plot import Plot, ArrayPlotData
 from chaco.api import ToolbarPlot
 from chaco.tools.api import PanTool, ZoomTool
 from enable.api import ComponentEditor
-from traits.api import Instance, HasTraits
+from traits.api import Array, Instance, HasTraits
 from traitsui.api import View, Item
 
 
@@ -28,14 +28,18 @@ class ExamplePlotApp(HasTraits):
         resizable=True,
     )
 
-    def __init__(self, index, series1, series2, **kw):
-        super(ExamplePlotApp, self).__init__(**kw)
+
+    def _plot_default(self):
+        index = numpy.arange(1.0, 10.0, 0.01)
+        series1 = (100.0 + index) / (100.0 - 20 * index ** 2 + 5.0 * index ** 4)
+        series2 = (100.0 + index) / (100.0 - 20 * index ** 2 + 5.0 * index ** 3)
+
         plot_data = ArrayPlotData(index=index)
         plot_data.set_data("series1", series1)
         plot_data.set_data("series2", series2)
 
-        self.plot = ToolbarPlot(plot_data)
-        line_plot = self.plot.plot(("index", "series1"), color="auto")[0]
+        plot = ToolbarPlot(plot_data)
+        line_plot = plot.plot(("index", "series1"), color="auto")[0]
 
         # Add pan and zoom tools
         line_plot.tools.append(PanTool(line_plot))
@@ -44,11 +48,11 @@ class ExamplePlotApp(HasTraits):
         # Set the domain_limits
         line_plot.index_mapper.domain_limits = (3.3, 6.6)
 
+        return plot
 
-index = numpy.arange(1.0, 10.0, 0.01)
-series1 = (100.0 + index) / (100.0 - 20 * index ** 2 + 5.0 * index ** 4)
-series2 = (100.0 + index) / (100.0 - 20 * index ** 2 + 5.0 * index ** 3)
-demo = ExamplePlotApp(index, series1, series2)
+
+
+demo = ExamplePlotApp()
 
 if __name__ == "__main__":
     demo.configure_traits()
