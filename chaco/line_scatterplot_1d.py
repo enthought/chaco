85bec73e1b9a44ae4291412e58bfe0d39d2709de
Aaron Ayres <aayres@enthought.com>
2021-05-17 11:29:27 -0500

    add stub files at old file locations which raise Deprecation warning and import objects from their new chaco/plots modules

---
+++ chaco/line_scatterplot_1d.py
@@ -0,0 +1,19 @@
+# (C) Copyright 2006-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+"""
+A 1D scatterplot that draws lines across the renderer at the index values
+"""
+import warnings
+warnings.warn(
+    "This module has been deprecated, please import from chaco.api instead",
+    DeprecationWarning
+)
+
+from chaco.plots.line_scatterplot_1d import LineScatterPlot1D
