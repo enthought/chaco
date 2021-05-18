6a582e46f8a9862af4f1183441dd1b2c5e5f8423
Aaron Ayres <aayres@enthought.com>
2021-05-18 12:32:14 -0500

    remove all definitions of _draw_component

--- chaco/base_xy_plot.py
+++ chaco/base_xy_plot.py
@@ -474,14 +474,8 @@ class BaseXYPlot(AbstractPlotRenderer):
 
     def _draw_plot(self, gc, view_bounds=None, mode="normal"):
         """Draws the 'plot' layer."""
-        self._draw_component(gc, view_bounds, mode)
-
-    def _draw_component(self, gc, view_bounds=None, mode="normal"):
-        # This method should be folded into self._draw_plot(), but is here for
-        # backwards compatibilty with non-draw-order stuff.
-
         pts = self.get_screen_points()
-        self._render(gc, pts)
+        self._render(gc, pts)   
 
     def _draw_default_axes(self, gc):
         if not self.origin_axis_visible:
