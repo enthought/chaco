5a3bbad3a02b6fe9eafb076de6d4fd5e22612d1f
Poruri Sai Rahul <rporuri@enthought.com>
2021-06-03 16:24:25 +0000

    Add/Update copyright headers (#755)
    * DEV : Add/update copyright headers
    
    this commit adds copyright headers to files that dont have them and it
    updates copyright headers that do have them but have the wrong
    format/wrong year range etc.
    
    note that the year range has been updated to 2005-2021 - and there is
    only one instance of an older year range and it was 2002
    
    * DEV : Update year range in license file
    
    	modified:   LICENSE.txt

--- chaco/ui/axis_ui.py
+++ chaco/ui/axis_ui.py
@@ -1,3 +1,13 @@
+# (C) Copyright 2005-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
 """ This file contains the ui specifications for the chaco.axis.Axis objects.
 
     Much of it is defined in re-usable chunks so that elements of it can be
