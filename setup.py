9d0dc74eafe2d1f1f4ee4eb8e860e133c02077f7
Aaron Ayres <aayres@enthought.com>
2021-06-03 07:41:38 -0500

    flip IS_RELEASED back to false for continued development

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ MAJOR = 5
 MINOR = 0
 MICRO = 0
 PRERELEASE = "rc1"
-IS_RELEASED = True
+IS_RELEASED = False
 
 # If this file is part of a Git export (for example created with "git archive",
 # or downloaded from GitHub), ARCHIVE_COMMIT_HASH gives the full hash of the
