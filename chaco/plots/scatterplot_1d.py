ea1d2eb18863dcd2a973f9547d6d77e1c29e4c61
Chengyu Liu <cyliu@aus552cyliu.local>
2023-03-13 17:17:27 +0900

    FORMAT: use Union to have more specific traits for marker size

--- chaco/plots/scatterplot_1d.py
+++ chaco/plots/scatterplot_1d.py
@@ -17,7 +17,7 @@ from numpy import empty
 
 # Enthought library imports
 from enable.api import black_color_trait, ColorTrait, MarkerTrait
-from traits.api import Any, Bool, Callable, Enum, Float, Str
+from traits.api import Any, Array, Bool, Callable, Enum, Float, Int, Str, Union
 
 # local imports
 from chaco.base_1d_plot import Base1DPlot
@@ -32,7 +32,7 @@ class ScatterPlot1D(Base1DPlot):
     marker = MarkerTrait
 
     # The pixel size of the marker, not including the thickness of the outline.
-    marker_size = Any
+    marker_size = Union(Float, Int, Array(Float), Array(Int), None)
 
     # The CompiledPath to use if **marker** is set to "custom". This attribute
     # must be a compiled path for the Kiva context onto which this plot will
