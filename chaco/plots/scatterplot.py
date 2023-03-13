3e71256bbe0d1bfd169aa534a360f91eac22b792
Chengyu Liu <cyliu@aus552cyliu.local>
2023-03-13 13:35:04 +0900

    add marker size check

--- chaco/plots/scatterplot.py
+++ chaco/plots/scatterplot.py
@@ -149,6 +149,10 @@ def render_markers(
 
         gc.begin_path()
 
+        # check for None marker size and give a default value
+        if marker_size is None:
+            marker_size = 4.0
+
         # try to invoke optimized routines if only one size and gc supports
         if not isinstance(marker_size, ndarray):
             # try fastest routine
