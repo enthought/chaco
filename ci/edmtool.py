2f7de50c57edb13eebd94b887c6bb1e14b69c164
Chengyu Liu <cyliu@aus552cyliu.local>
2023-03-16 16:26:50 +0900

    add 3.11 test package

--- ci/edmtool.py
+++ ci/edmtool.py
@@ -79,7 +79,7 @@ import click
 supported_combinations = {
     '3.6': {'pyside2', 'pyqt5', 'null'},
     '3.8': {'pyside6', 'null'},
-    '3.8': {'pyside6', 'null'},
+    '3.11': {'pyside6', 'null'},
 }
 
 dependencies = {
