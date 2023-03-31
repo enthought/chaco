2a61adef0a97bea2f252e75a7f482de3da7d3c74
Chengyu Liu <cyliu@aus552cyliu.local>
2023-03-31 10:33:34 +0900

    MAINT: fix float input for number of samples

--- examples/demo/advanced/spec_waterfall.py
+++ examples/demo/advanced/spec_waterfall.py
@@ -100,7 +100,7 @@ class WaterfallRenderer(LinePlot):
 
 def _create_plot_component(obj):
     # Setup the spectrum plot
-    frequencies = linspace(0.0, float(SAMPLING_RATE) / 2, num=NUM_SAMPLES / 2)
+    frequencies = linspace(0.0, float(SAMPLING_RATE) / 2, num=NUM_SAMPLES // 2)
     obj.spectrum_data = ArrayPlotData(frequency=frequencies)
     empty_amplitude = zeros(NUM_SAMPLES // 2)
     obj.spectrum_data.set_data("amplitude", empty_amplitude)
