c0ddb452b40c26a4a0c1b12c984c5854c7d29ac9
Chengyu Liu <cyliu@aus552cyliu.local>
2023-04-10 16:06:14 +0900

    MAINT: add different import for resources under different python versions

--- examples/user_guide/plot_types/create_plot_snapshots.py
+++ examples/user_guide/plot_types/create_plot_snapshots.py
@@ -434,9 +434,15 @@ def get_image_plot():
 
 
 def get_image_from_file():
-    from importlib import resources
+    # importlib.resources is new in Python 3.7, and importlib.resources.files is
+    # new in Python 3.9, so for Python < 3.9 we must rely on the 3rd party
+    # importlib_resources package.
+    try:
+        from importlib.resources import files
+    except ImportError:
+        from importlib_resources import files
 
-    filename = resources.files(
+    filename = files(
         'chaco.examples.demo.basic'
     ).joinpath('capitol.jpg')
     image_source = ImageData.fromfile(filename)
