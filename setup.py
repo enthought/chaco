666a7c36f2f6404a968a618aca9282ca098d8fe8
Ioannis Tziakos <ioannist@enthought.com>
2025-10-17 15:02:21 +0100

    Fix the release pypi workflow (#922)
    - Update pypa and docker actions to the latest
    - Update skipped python versions
    - Update build archs for macos
    - Do not upload to pypi when the workflow is manually triggered (helps
    with debugging the worfklow)
    - Update minimum Python requirement for chaco (it should be 3.10)

--- setup.py
+++ setup.py
@@ -367,5 +367,5 @@ if __name__ == "__main__":
         packages=find_packages(),
         platforms=["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
         zip_safe=False,
-        python_requires=">=3.9",
+        python_requires=">=3.10",
     )
